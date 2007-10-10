af2aaea8ea0b032997f69973740fba4c3676840a
Nicolas Neri <neri@lrde.epita.fr>
2007-10-10 09:48:31 +0000

    Add test for translate image.
    	* translate_image.cc: New test.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1288 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/tests/translate_image.cc
@@ -0,0 +1,76 @@
+// Copyright (C) 2007 EPITA Research and Development Laboratory
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+/*! \file tests/translate_image.cc
+ *
+ * \brief Tests on mln::core::translate_image.hh.
+ */
+
+#include <mln/core/image2d.hh>
+#include <mln/value/int_u8.hh>
+#include <mln/debug/iota.hh>
+#include <mln/level/fill.hh>
+#include <mln/level/paste.hh>
+#include <mln/border/fill.hh>
+#include <mln/debug/println_with_border.hh>
+#include <mln/debug/println.hh>
+#include <mln/core/translate_image.hh>
+
+
+int main ()
+{
+  using namespace mln;
+
+  typedef image2d<value::int_u8> I;
+
+  I ima (4, 2, 1);
+  debug::iota (ima);
+  translate_image<I> tmp (ima, make::point2d (0,2) - make::point2d (0,0));
+  std::cout << "orginal image domain : "
+	    << ima.domain ()
+	    << std::endl
+	    << "translated image domain : "
+	    << tmp.domain ()
+	    << std::endl;
+
+  std::cout << "original image :"
+	    << std::endl;
+  debug::println (ima);
+  std::cout << std::endl;
+  std::cout << "translated image :"
+	    << std::endl;
+  debug::println (tmp);
+
+  std::cout << std::endl;
+  I out (4,4);
+  level::paste(ima, out);
+  level::paste(tmp, out);
+  std::cout << "pasted image :"
+	    << std::endl;
+  debug::println (out);
+  std::cout << std::endl;
+}
