0e4ef5033d369c2ac66f82418268455dec3c114f
Nicolas Neri <neri@lrde.epita.fr>
2007-10-11 15:30:41 +0000

    Fix bug in translate_image.
    	* translate_image.hh: Fix.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1316 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/mln/core/translate_image.hh
+++ milena/mln/core/translate_image.hh
@@ -76,18 +76,18 @@ namespace mln
 
       typedef trait::image::category::domain_morpher category;
 
-//       typedef mln_trait_image_access_from(I)   access;
-//       typedef mln_trait_image_space_from(I)    space;
-//       typedef mln_trait_image_size_from(I)     size;
-//       typedef mln_trait_image_support_from(I)  support;
+//       typedef mln_trait_image_access(I)()   access;
+//       typedef mln_trait_image_space(I)()    space;
+//       typedef mln_trait_image_size(I)()     size;
+//       typedef mln_trait_image_support(I)()  support;
 
-//       typedef mln_trait_image_border_from(I)   border;
-      typedef mln_trait_image_data_from(I)     data;
-      typedef mln_trait_image_io_from(I)       io;
-//       typedef mln_trait_image_speed_from(I)    speed;
+//       typedef mln_trait_image_border(I)()   border;
+//       typedef mln_trait_image_data(I)()     data;
+//       typedef mln_trait_image_io(I)()       io;
+//       typedef mln_trait_image_speed(I)()    speed;
 
-//       typedef mln_trait_image_io_from_(I) io;
-//       typedef mln_trait_image_data_from_(I) data;
+      typedef mln_trait_image_io_from_(I) io;
+      typedef mln_trait_image_data_from_(I) data;
 
     };
 
@@ -106,6 +106,7 @@ namespace mln
   {
 
     typedef mln::internal::image_morpher_< I, mln_pset(I), translate_image<I> > super_;
+    typedef line_piter_<mln_psite(I)> line_piter;
 
     /// Return type of read-write access.
     typedef typename internal::morpher_lvalue_<I>::ret lvalue;
