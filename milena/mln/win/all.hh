3b2f125ab2eff34047ba7caff6c9b7b1cd6f642c
Nicolas Neri <neri@lrde.epita.fr>
2007-10-17 10:25:11 +0000

    Update typo.
    	* level/approx/all.hh,
    	* level/naive/all.hh,
    	* tag/all.hh,
    	* win/all.hh: New file for doc namespace.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1345 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/mln/win/all.hh
@@ -0,0 +1,58 @@
+// Copyright (C) 2007 EPITA Research and Development Laboratory
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+#ifndef MLN_WIN_ALL_HH
+# define MLN_WIN_ALL_HH
+
+/*! \file mln/win/all.hh
+ *
+ * \brief File that includes all win-related routines.
+ */
+
+
+namespace mln
+{
+
+  /// Namespace of image processing routines related to win.
+  namespace win {}
+
+}
+
+# include <mln/win/backdiag2d.hh>
+# include <mln/win/cube3d.hh>
+# include <mln/win/diag2d.hh>
+# include <mln/win/disk2d.hh>
+# include <mln/win/hline2d.hh>
+# include <mln/win/line.hh>
+# include <mln/win/octagon2d.hh>
+# include <mln/win/rectangle2d.hh>
+# include <mln/win/segment1d.hh>
+# include <mln/win/vline2d.hh>
+
+
+
+#endif // ! MLN_WIN_ALL_HH
