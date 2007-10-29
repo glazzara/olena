0afc8d1fb081aee92686ff8ea5a2e328587acf15
Nicolas Neri <neri@lrde.epita.fr>
2007-10-29 10:38:42 +0000

    Update test for fllt.
    	* test_fllt.cc: Update.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1401 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/garrigues/test_fllt.cc
+++ milena/sandbox/garrigues/test_fllt.cc
@@ -9,32 +9,40 @@
 # include <mln/level/fill.hh>
 # include <mln/io/pgm/load.hh>
 # include <mln/io/pbm/load.hh>
+# include <sstream>
+
 
 int main()
 {
 
   using namespace mln;
 
-  int ws[81] = {3,2,3,3,5,5,5,5,5,
-		2,1,3,4,4,4,4,5,5,
-		2,3,4,2,3,3,2,4,4,
-		1,4,2,1,1,2,1,2,2,
-		1,2,4,2,1,2,1,1,1,
-		1,3,3,3,4,3,2,5,1,
-		1,3,4,3,4,3,2,5,1,
-		1,3,3,3,4,3,2,5,1,
-		1,3,3,4,2,3,2,1,1};
+//   int ws[81] = {3,2,3,3,5,5,5,5,5,
+// 		2,1,3,4,4,4,4,5,5,
+// 		2,3,4,2,3,3,2,4,4,
+// 		1,4,2,1,1,2,1,2,2,
+// 		1,2,4,2,1,2,1,1,1,
+// 		1,3,3,3,4,3,2,5,1,
+// 		1,3,4,3,4,3,2,5,1,
+// 		1,3,3,3,4,3,2,5,1,
+// 		1,3,3,4,2,3,2,1,1};
 
-  w_window2d_int w_win = make::w_window2d(ws);
- image2d<int> ima = convert::to_image(w_win);
-fllt::fllt(ima);
+//   w_window2d_int w_win = make::w_window2d(ws);
+//   image2d<int> ima = convert::to_image(w_win);
+//   fllt::fllt(ima);
 
 
-//    image2d<value::int_u8> ima = io::pgm::load("../../img/tiny.pgm");
+  for (int i = 1; i < 16; ++i)
+    for (int j = 1; j < 16; ++j)
+      {
+	std::stringstream path;
+	path << "/lrde/tegucigalpa/theo/pub/mln_docs/lena_tiles/lena_" << i << "_" << j << ".pgm";
+	std::cout << path.str () << std::endl;
+	image2d<value::int_u8> ima = io::pgm::load(path.str());
+	image2d<int> ima_int(ima.domain());
+	level::fill(ima_int, ima);
+	debug::println(ima_int);
+	fllt::fllt(ima_int);
+      }
 
-//    image2d<int> ima_int(ima.domain());
-
-// level::fill(ima_int, ima);
-//   debug::println(ima);
-//   fllt::fllt(ima_int);
 }
