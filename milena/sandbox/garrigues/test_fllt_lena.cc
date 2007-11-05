00938b324fb7b953862708a1355b2c840a67d780
Matthieu Garrigues <garrigues@lrde.epita.fr>
2007-11-05 13:45:04 +0000

    Add more tests for FLLT.
    	* sandbox/garrigues/test_fllt6.cc: New,
    	* sandbox/garrigues/test_fllt7.cc: New,
    	* sandbox/garrigues/test_fllt8.cc: New,
    	* sandbox/garrigues/test_fllt9.cc: New,
    	* sandbox/garrigues/test_fllt_lena.cc: New,
    	* sandbox/garrigues/test_fllt_lena_tiles.cc: New, test FLLT with
              differents 2d images.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1427 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/sandbox/garrigues/test_fllt_lena.cc
@@ -0,0 +1,24 @@
+# include "fllt.hh"
+# include <mln/core/image2d.hh>
+# include <mln/core/clone.hh>
+# include <mln/value/int_u8.hh>
+# include <mln/debug/println.hh>
+# include <mln/convert/to_w_window.hh>
+# include <mln/core/w_window2d_int.hh>
+# include <mln/convert/to_image.hh>
+# include <mln/level/fill.hh>
+# include <mln/io/pgm/load.hh>
+# include <mln/io/pbm/load.hh>
+
+int main()
+{
+
+  using namespace mln;
+
+   image2d<value::int_u8> ima = io::pgm::load("../../img/lena.pgm");
+
+   image2d<int> ima_int(ima.domain());
+
+   level::fill(ima_int, ima);
+   fllt::fllt(ima_int);
+}
