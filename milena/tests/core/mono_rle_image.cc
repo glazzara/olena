65db73d887fe3807e36b900268711a55bbf07bb8
Simon Nivault <nivault@lrde.epita.fr>
2007-12-03 16:44:13 +0000

    Generate mono_rle_image and add his test.
    	* mln/core/mono_rle_encode.hh: New.
    	* mln/core/mono_rle_image.hh: New.
    	* tests/core/mono_rle_image.cc: New.
    	* tests/core/Makefile.am: Add test.
    	* tests/core/rle_image.cc: Fix typo.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1583 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/tests/core/mono_rle_image.cc
+++ milena/tests/core/mono_rle_image.cc
@@ -25,9 +25,9 @@
 // reasons why the executable file might be covered by the GNU General
 // Public License.
 
-/*! \file tests/core/rle_image.cc
+/*! \file tests/mono_rle_iimage.cc
  *
- * \brief Test on mln::rle_image.hh.
+ * \brief Test on mln::labeling::blobs.
  */
 
 #include <mln/core/image2d.hh>
@@ -42,7 +42,7 @@
 #include <mln/level/compare.hh>
 #include <mln/io/pgm/save.hh>
 
-#include <mln/core/rle_encode.hh>
+#include <mln/core/mono_rle_encode.hh>
 
 struct fold_t : public mln::Function_v2v< fold_t >
 {
@@ -50,7 +50,11 @@ struct fold_t : public mln::Function_v2v< fold_t >
   result operator()(unsigned i) const { return i == 0 ? 0 : (i - 1) % 255 + 1; }
 };
 
-
+struct only_two_t : public mln::Function_v2v< only_two_t >
+{
+  typedef mln::value::int_u8 result;
+  result operator()(unsigned i) const { return i == 2 ? 2 : 0; }
+};
 
 int main()
 {
@@ -66,10 +70,10 @@ int main()
 					     c4(), n);
   std::cout << n << std::endl;
 
-  rle_image<point2d, int_u8> rle = rle_encode(level::transform(labels, fold_t()));
+  mono_rle_image<point2d, int_u8> rle = mono_rle_encode(level::transform(labels, fold_t()), 2);
 
   level::fill(cmp, literal::zero);
   level::paste(rle, cmp);
 
-  mln_assertion(cmp == level::transform(labels, fold_t()));
+  mln_assertion(cmp == level::transform(labels, only_two_t()));
 }
