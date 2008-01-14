fd4eac009a72f301ea1f3a118ba76d341ceddd25
Roland Levillain <roland@lrde.epita.fr>
2008-01-14 18:03:18 +0000

    Speed up execution times of tests on the median operators.
    	* tests/tests.mk (LONG_TESTS_CXXFLAGS): New.
    	Set to -O3.
    	* tests/level/approx/Makefile.am, tests/level/naive/Makefile.am
    	(median_CXXFLAGS): New.
    	Set to $(LONG_TESTS_CXXFLAGS).
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1659 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/tests/tests.mk
+++ milena/tests/tests.mk
@@ -8,7 +8,11 @@ AM_CPPFLAGS = -I$(top_srcdir)/milena -I$(top_builddir)/milena
 #
 #   AM_CXXFLAGS = $(CXXFLAGS_STRICT) $(CXXFLAGS_DEBUG)
 #
-# when oln.m4 is available in the distribution, instead of using the
-# hand-made TESTS_CXXFLAGS.
+# when oln.m4 (or just a part of it) is available in the distribution,
+# instead of using the hand-made TESTS_CXXFLAGS.
 TESTS_CXXFLAGS = @TESTS_CXXFLAGS@
 AM_CXXFLAGS = $(TESTS_CXXFLAGS)
+
+# FIXME: Likewise, we should compute these values at configure time.
+# Hard-code them for the moment.
+LONG_TESTS_CXXFLAGS = -O3
