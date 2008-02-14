dff3b01cae409aa95ae156eb8254e944cefbe57e
Étienne Folio <folio@lrde.epita.fr>
2008-02-14 16:56:47 +0000

    First work on naive dt.
    	* sandbox/folio/dt_naive.cc: Test file, in progress
    	* sandbox/folio/dt_naive.hh: Algorithm
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1724 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/sandbox/folio/dt_naive.cc
@@ -0,0 +1,49 @@
+// Copyright (C) 2007, 2008 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+/*! \file tests/linear_gaussian.cc
+ *
+ * \brief Test on mln::linear::gaussian.
+ */
+
+#include "dt_naive.hh"
+
+int main()
+{
+  using namespace mln;
+
+  image2d< value::int_u8 > lena;
+  io::pgm::load(lena, MLN_IMG_DIR "/lena.pgm");
+
+//   image2d<float> tmp(lena.domain());
+//   linear::gaussian(lena, 5.1f, tmp);
+
+//   image2d< value::int_u_sat<8> > out(lena.domain());
+//   level::transform(tmp, math::round<int>(), out);
+//   io::pgm::save(out, "out.pgm");
+ }
+}
