493e11e72bba667a3beaae47c55b6e97109965f1
Ugo Jardonnet <jardonnet@lrde.epita.fr>
2008-04-03 16:53:06 +0000

    Sandboc : ICP: Update check script (gprof).
    	* sandbox/jardonnet/test/bench: Add info.
    	* sandbox/jardonnet/test/icp_check.sh: Update testes.
    	* sandbox/jardonnet/test/Makefile: Update rule for gprof.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1838 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/jardonnet/test/icp_check.sh
+++ milena/sandbox/jardonnet/test/icp_check.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 for i in `\ls bin/`
 do
-    echo execute $i 01.pbm 02.pbm
-    time ./bin/$i 01.pbm 02.pbm > ./bin/log_$i
-    echo ./bin/log_$i
+    echo "  " $i
+    \time -f "%U" ./bin/$i 01.pbm 02.pbm > ./bin/log_$i
+    echo ""
 done
