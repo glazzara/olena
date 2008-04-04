11196c7ba6f01d6e4ede69ae439f95921fb87477
Matthieu Garrigues <garrigues@lrde.epita.fr>
2008-04-04 15:04:52 +0000

    Add bounding box computation to p_image2d.
    	* mln/core/p_image2d.hh:
    	(p_image2d<P>::box_<mln_point(P)>& bbox()): New, give the exact
    	boungind box.
    	(p_image2d<P>::insert(const p_image2d& set)): Rename as..
    	(p_image2d<P>::insert(const Point_Set<S>& set)): ...this, and update it
    	to be more generic.
    	(p_image2d<P>::remove(const p_image2d& set)): Rename as..
    	(p_image2d<P>::remove(const Point_Set<S>& set)): ...this, likewise.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1839 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/mln/core/p_image2d.hh
+++ milena/mln/core/p_image2d.hh
@@ -37,9 +37,7 @@
 # include <mln/core/internal/point_set_base.hh>
 # include <mln/core/box2d.hh>
 # include <mln/core/image2d.hh>
-# include <mln/accu/bbox.hh>
 # include <mln/level/fill.hh>
-# include <mln/core/p_array.hh>
 
 
 namespace mln
@@ -67,11 +65,13 @@ namespace mln
 
     /// Insert a point \p p.
     p_image2d<P>& insert(const P p);
-    p_image2d<P>& insert(const p_image2d& set);
+    template <typename S>
+    p_image2d<P>& insert(const Point_Set<S>& set);
 
     /// Remove a point \p p.
     p_image2d<P>& remove(const P p);
-    p_image2d<P>& remove(const p_image2d& set);
+    template <typename S>
+    p_image2d<P>& remove(const Point_Set<S>& set);
 
     /// Give the number of points.
     unsigned npoints() const;
@@ -82,6 +82,9 @@ namespace mln
     /// Test if the set is empty.
     bool is_empty() const;
 
+    /// Give the exact bounding box.
+    const box_<mln_point(P)>& bbox();;
+
     /// Clear this set.
     void clear();
 
@@ -90,113 +93,143 @@ namespace mln
   private:
     image2d<bool> points_;
     unsigned npoints_;
+    accu::bbox<P> bb_;
+    bool bb_need_update_;
   };
 
 
 # ifndef MLN_INCLUDE_ONLY
 
-    template <typename P>
-    p_image2d<P>::p_image2d(int nrows, int ncols)
-      : points_(nrows, ncols),
-	npoints_(0)
-    {
+  template <typename P>
+  p_image2d<P>::p_image2d(int nrows, int ncols)
+    : points_(nrows, ncols),
+      npoints_(0),
+      bb_need_update_(false)
+  {
 
-      level::fill(points_, false);
+    level::fill(points_, false);
+  }
+
+  template <typename P>
+  p_image2d<P>::p_image2d(const box2d& b)
+    : points_(b),
+      npoints_(0),
+      bb_need_update_(false)
+  {
+    level::fill(points_, false);
+  }
+
+
+  template <typename P>
+  p_image2d<P>&
+  p_image2d<P>::insert(const P p)
+  {
+    if (points_(p) == false)
+    {
+      bb_.take(p);
+      points_(p) = true;
+      npoints_++;
     }
+    return *this;
+  }
 
-    template <typename P>
-    p_image2d<P>::p_image2d(const box2d& b)
-      : points_(b),
-	npoints_(0)
+  template <typename P>
+  template <typename S>
+  p_image2d<P>&
+  p_image2d<P>::insert(const Point_Set<S>& set_)
+  {
+    const S& set = exact(set_);
+    mln_fwd_piter(S) p(set);
+    for_all(p)
+      if (this->points_.has(p))
+	this->insert(p);
+    return *this;
+  }
+
+  template <typename P>
+  p_image2d<P>&
+  p_image2d<P>::remove(const P p)
+  {
+    if (points_(p) == true)
     {
-      level::fill(points_, false);
+      points_(p) = false;
+      bb_need_update_ = true;
+      npoints_--;
     }
+    return *this;
+  }
 
-
-    template <typename P>
-    p_image2d<P>&
-    p_image2d<P>::insert(const P p)
-    {
-      if (points_(p) == false)
-      {
-	points_(p) = true;
-	npoints_++;
-      }
+  template <typename P>
+  template <typename S>
+  p_image2d<P>&
+  p_image2d<P>::remove(const Point_Set<S>& set_)
+  {
+    const S& set = exact(set_);
+    if (this->is_empty())
       return *this;
-    }
+    mln_fwd_piter(S) p(set);
+    for_all(p)
+      if (this->points_.has(p))
+	this->remove(p);
+    return *this;
+  }
 
-    template <typename P>
-    p_image2d<P>&
-    p_image2d<P>::insert(const p_image2d& set)
+  template <typename P>
+  unsigned
+  p_image2d<P>::npoints() const
+  {
+    return npoints_;
+  }
+
+  template <typename P>
+  bool
+  p_image2d<P>::has(const P& p) const
+  {
+    return points_(p);
+  }
+
+  template <typename P>
+  bool
+  p_image2d<P>::is_empty() const
+  {
+    return npoints_ == 0;
+  }
+
+
+  template <typename P>
+  inline
+  const box_<mln_point(P)>&
+  p_image2d<P>::bbox()
+  {
+    mln_precondition(npoints() != 0);
+    if (bb_need_update_)
     {
-      if (set->is_empty())
-	return *this;
-      mln_fwd_piter(image2d<bool>) p(set.points_ | true);
+      bb_.init();
+
+      mln_fwd_piter(p_image2d<P>) p(*this);
       for_all(p)
-	if (this->points_.has(p))
-	  this->insert(p);
-      return *this;
+	bb_.take(p);
+      bb_need_update_ = false;
     }
 
-    template <typename P>
-    p_image2d<P>&
-    p_image2d<P>::remove(const P p)
-    {
-      if (points_(p) == true)
-      {
-	points_(p) = false;
-	npoints_--;
-      }
-      return *this;
-    }
+    return bb_.to_result();
+  }
 
-    template <typename P>
-    p_image2d<P>&
-    p_image2d<P>::remove(const p_image2d& set)
-    {
-      if (this->is_empty() || set->is_empty())
-	return *this;
-      mln_fwd_piter(image2d<bool>) p(set.points_ | true);
-      for_all(p)
-	if (this->points_.has(p))
-	  this->remove(p);
-      return *this;
-    }
+  template <typename P>
+  void
+  p_image2d<P>::clear()
+  {
+    level::fill(points_, false);
+    bb_.init();
+    bb_need_update_ = false;
+  }
 
-    template <typename P>
-    unsigned
-    p_image2d<P>::npoints() const
-    {
-      return npoints_;
-    }
-
-    template <typename P>
-    bool
-    p_image2d<P>::has(const P& p) const
-    {
-      return points_(p);
-    }
-
-    template <typename P>
-    bool
-    p_image2d<P>::is_empty() const
-    {
-      return npoints_ == 0;
-    }
-
-    template <typename P>
-    void
-    p_image2d<P>::clear()
-    {
-      level::fill(points_, false);
-    }
-
-    template <typename P>
-    const image2d<bool>&
-    p_image2d<P>::image() const
-    {
-      return points_;
-    }
+  template <typename P>
+  const image2d<bool>&
+  p_image2d<P>::image() const
+  {
+    return points_;
+  }
 
 # endif // ! MLN_INCLUDE_ONLY
 
