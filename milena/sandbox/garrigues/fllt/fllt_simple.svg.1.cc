0d72c2ba1cfb2961550cc57f309d4de3e7d508b6
Matthieu Garrigues <garrigues@lrde.epita.fr>
2008-04-20 19:19:19 +0000

    Test a simple fllt.
    	* sandbox/garrigues/fllt/fllt_simple.cc: Start to construct
    	shapes by browsing level lines in interpixels.
    	* sandbox/garrigues/fllt/fllt_simple.svg.1.cc: New, just browse the
    	c4 connected component of an image.
    	* sandbox/garrigues/fllt/fllt_simple.svg.2.cc: New, just browse the
    	c6 connected component of an image.
    	* sandbox/garrigues/fllt/fllt_simple.svg.3.cc: New, build a tree where
    	a connected component is parent of the shapes contained in its
    	holes. It give the same representation than fllt only on simple images.
    	* sandbox/garrigues/fllt/fllt_theo.cc: Old work, a faster version is
    	in sandbox/geraud/fllt.cc
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1877 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/garrigues/fllt/fllt_simple.svg.1.cc
+++ milena/sandbox/garrigues/fllt/fllt_simple.svg.1.cc
@@ -30,6 +30,7 @@
 #include <sstream>
 
 #include <mln/core/image2d.hh>
+#include <mln/core/sub_image.hh>
 #include <mln/core/neighb2d.hh>
 #include <mln/core/p_array.hh>
 #include <mln/core/clone.hh>
@@ -50,21 +51,68 @@
 
 namespace mln
 {
+  // Un autre version recursive de la fllt.
+
+  // Idee :
+  // Parcourir l'image en pratant d'un coin de preference,
+  // le parcour des pixels se fait dans un ordre conexe.
+  //
+
+  /*
+
+   fonction fllt_rec(racine R, domaine D, marques M, conexite C)
+   {
+     pour tout les points du domaine non marquees
+       recuperer la composante conexe A (en conexite C) qui inclut ce point :
+            Ceci est fait avec la methode de grossissement de region
+            pour avoir acces a la bordure externe et donc aux trous
+            de la composante.
+
+       Cette composante sera fille de R dans l'arbre d'inclusion.
+
+       Marquer les points de A (Peut etre fait en meme temps que la
+       construction de A)
+
+       Pour chaque trous T de A :
+         appel recursif : fllt_rec(A, T.domaine(), marques, inv(C))
+         T.domaine() sera calculer en recuperant la composante connexe
+         de la bordure de A lui correspondant (On recupere facilement
+	 les segments horizontaux qui le composent -> parcour ok).
+
+   }
+   inv(c4) = c8
+   inv(c8) = c4
+
+
+    Autre version qui Marche sur les images hexagonales (Plus rapide et plus simple)
+    car il y a plus le probleme du theoreme de jordan non verifie.
+    FIXME: Est-ce que rendre l'image hexagonale altere vraiment le resultat?
+
+    pour tout les points du domaine non marquees
+       recuperer la composante conexe A qui inclut ce point :
+            Ceci est fait avec la methode de grossissement de region
+            pour avoir acces a la bordure externe et donc aux trous
+            de la composante.
+
+       Marquer les points de A (Peut etre fait en meme temps que la
+       construction de A)
+
+       Marquer les extremites des trou de A.
+
+       Si un point de A est l'extremite d'un trou,
+           alors on a facilement le pere de A (la forme qui inclue A).
+       Sinon
+           C'est qu'il y a une composante connexe adjacente qui
+	   a deja ete construite, et qui a donc deja un pere, qui
+	   est aussi celui de A.
+
+    continuer jusqu'a avoir marquer tout les points
+
+  */
 
   namespace my
   {
 
-
-    void save_state(const image2d<unsigned char>& out)
-    {
-//       static int id = 0;
-//       std::stringstream filename;
-//       filename << "fllt_trace_" << std::setw(5) << std::setfill('0')
-// 	       << std::right << id++ << ".ppm";
-
-//       io::pgm::save(cast_image<value::int_u8>(out), filename.str());
-      //io::pgm::save(out, filename.str());
-    }
 
     void save_u(const image2d<value::int_u8>& u,
 		const image2d<unsigned char>& is,
@@ -78,14 +126,20 @@ namespace mln
       image2d<value::int_u8> out = clone(u);
       const unsigned in_R = 255;
 
-      mln_assertion(R_box.is_valid());
       mln_piter_(box2d) p(R_box);
       for_all(p)
 	if (is(p) == in_R)
 	  out(p) = 255;
 
       io::pgm::save(out, filename.str());
-      //io::pgm::save(out, filename.str());
+    }
+
+    void swap_arrays(p_array<point2d>*& A,
+		     p_array<point2d>*& N)
+    {
+      p_array<point2d>* tmp = A;
+      A = N;
+      N = tmp;
     }
 
     template <typename I, typename Nbh>
@@ -93,10 +147,6 @@ namespace mln
     {
       const I& input = exact(input_);
       const Nbh& nbh = exact(nbh_);
-
-      unsigned l = 0, l_max;
-      mln_ch_value(I, unsigned) reg_min =
-	labeling::regional_minima(input, nbh, l_max);
 
       // Variables.
       I u = mln::clone(input);
@@ -107,10 +157,12 @@ namespace mln
       const unsigned in_R = 255, in_N = 2, in_O = 0;
 
       typedef p_array<mln_point(I)> arr_t;
-      arr_t A;
-      arr_t N[256];
+      arr_t* A = new arr_t();
+      arr_t* N = new arr_t();
 
-      accu::bbox<mln_point(I)> R_box;
+      accu::bbox<mln_point(I)> R_box, N_box;
+
+      unsigned n_step_1 = 0, n_step_3 = 0;
 
       level::fill(tagged, false);
       mln_piter(I) min(input.domain());
@@ -121,16 +173,10 @@ namespace mln
 #ifdef FLLTDEBUG
 	std::cout << "Step 1" << std::endl;
 #endif
-	if (l == l_max)
-	  return;
-	l += 1;
-	for(min.next(); min.is_valid(); min.next())
-	  if (reg_min(min) !=0 && !tagged(min))
+	++n_step_1;
+	for(; min.is_valid(); min.next())
+	  if (!tagged(min))
 	    break;
-
-#ifdef FLLTDEBUG
-	std::cout << "min local : " << min << std::endl;
-#endif
 
 	if (!min.is_valid())
 	  goto end;
@@ -145,143 +191,100 @@ namespace mln
 #ifdef FLLTDEBUG
 	std::cout << "Step 2" << std::endl;
 #endif
+	if (N_box.is_valid())
+	  level::fill(inplace(is | N_box.to_result()), in_O);
+
+	N_box.init();
 	R_box.init();
-	level::fill(is, in_O);
-	A.clear();
-	A.append(x0);
-	for (unsigned i = 0; i < 256; ++i)
-	  N[i].clear();
+	A->clear();
+	A->append(x0);
+	N->clear();
       }
 
       // Step 3.
     step_3:
       {
+	++n_step_3;
 #ifdef FLLTDEBUG
 	std::cout << "Step 3" << std::endl;
 #endif
-	mln_piter(arr_t) a(A);
+	mln_piter(arr_t) a(*A);
 	mln_niter(Nbh) x(nbh, a);
 
 	// Stop.
-	if (A.npoints() == 0)
+	if (A->npoints() == 0)
 	  goto end;
 
 	// R <- R U A
 #ifdef FLLTDEBUG
-	      std::cout << "Add To R : ";
+	std::cout << "Add To R : ";
 #endif
 	for_all(a)
-	  {
-	    tagged(a) = true;
-	    is(a) = in_R;
+	{
+	  tagged(a) = true;
+	  is(a) = in_R;
 #ifdef FLLTDEBUG
-	      std::cout << a;
+	  std::cout << a;
 #endif
-	  }
+	}
 #ifdef FLLTDEBUG
 	std::cout << std::endl;
 #endif
 
 
 #ifdef FLLTDEBUG
-	std::cout << "points of A : " << A.npoints() << std::endl;
+	std::cout << "points of A : " << A->npoints() << std::endl;
 #endif
-	mln_assertion(A.npoints() > 0);
-	R_box.take(A.bbox());
+	mln_assertion(A->npoints() > 0);
+	R_box.take(A->bbox());
 	mln_assertion(R_box.is_valid());
 
 #ifdef FLLTTRACE
-	save_state(is);
 	save_u(u, is, R_box);
 #endif
-	// N <- N U { x in nbh of A and not in R }
+	// N <- N U { x in nbh of A and not in R / input(x) == g}
 #ifdef FLLTDEBUG
-	      std::cout << "Add To N : ";
+	std::cout << "Add To N : ";
 #endif
 	for_all(a)
 	  for_all(x)
-  	    if (u.has(x) && (is(x) == in_O))
-	    {
-	      is(x) = in_N;
-	      N[u(x)].append(x);
+	  if (u.has(x) && is(x) == in_O && input(x) == g)
+	  {
+	    mln_assertion(!tagged(x));
+	    N_box.take(x);
+	    is(x) = in_N;
+	    N->append(x);
 #ifdef FLLTDEBUG
-	      std::cout << x;
+	    std::cout << x;
 #endif
-	    }
+	  }
 #ifdef FLLTDEBUG
 	std::cout << std::endl;
 #endif
-	// gN = min u(x) for all x in N
-	unsigned i;
-	for (i = 0; i < 256; ++i)
-	  if (N[i].npoints() != 0)
-	  {
-	    gN = i;
-	    break;
-	  }
+
 #ifdef FLLTDEBUG
-	std::cout << "gN = " << gN << ", g = " << g << std::endl;
+	std::cout << "g = " << g << std::endl;
 #endif
 
 	// Stop if N is empty.
-#ifdef FLLTDEBUG
-	std::cout << "i =   " << i << std::endl;
-#endif
-	if (i == 256)
-	  goto step_4c;
-	mln_assertion(N[gN].npoints() > 0);
-
-	// FIXME: update the number of CC of the border of R
+	if (N->npoints() == 0)
+	  goto step_1;
+	else
+	{
+	  swap_arrays(A, N);
+	  N->clear();
+	  goto step_3;
+	}
       }
 
-      // Step 4.
     step_4:
       {
-	// a)
-	if (g < gN)
-	{
-#ifdef FLLTDEBUG
-	  std::cout << "Step 4a" << std::endl;
-#endif
-	  // FIXME: DO the hole thing.
-
-	  g = gN;
-
-	  A = N[gN];
-	  N[gN].clear();
-	  mln_assertion(A.npoints() > 0);
-	  mln_assertion(N[gN].npoints() == 0);
-	  goto step_3;
-	}
-	// b)
-	else if (g == gN)
-	{
-#ifdef FLLTDEBUG
-	  std::cout << "Step 4b" << std::endl;
-#endif
-	  A = N[gN];
-	  N[gN].clear();
-	  mln_assertion(A.npoints() > 0);
-	  mln_assertion(N[gN].npoints() == 0);
-	  goto step_3;
-	}
-	// c)
-	else
-	  {
-	  step_4c:
-#ifdef FLLTDEBUG
-	    std::cout << "Step 4c" << std::endl;
-#endif
-	    mln_assertion(R_box.is_valid());
-	    mln_piter(box_<mln_point(I)>) p(R_box);
-	    for_all(p)
-	      if (is(p) == in_R)
-		u(p) = g;
-	    goto step_1;
-	  }
+	// Follow N, find the holes, and browse it.
       }
+
     end:
       {
+	std::cout << n_step_1 << ' ' << n_step_3 << std::endl;
       }
     }
 
@@ -297,22 +300,22 @@ int main()
 
   image2d<int_u8> lena;
 
-  io::pgm::load(lena, "small.pgm");
+  io::pgm::load(lena, "../../../img/lena.pgm");
 
 
-//   int_u8 vs[9][9] = {
+  //   int_u8 vs[9][9] = {
 
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
-//     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
+  //     {  2, 2, 2, 2, 2, 2, 2, 2, 2  },
 
-//   };
+  //   };
 
   //image2d<int_u8> lena = make::image2d(vs);
 
