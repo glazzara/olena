5b7405c4d8818a9557ed465cb7370d73bb051caa
Caroline Vigouroux <vigouroux@lrde.epita.fr>
2008-04-22 11:56:23 +0000

    Add green and blue getters.
    	* cmy/get_blue.hh, cmy/get_green.hh,
    	  hsi/get_blue.hh, hsi/get_green.hh,
    	  xyz/get_blue.hh, xyz/get_green.hh,
    	  yiq/get_blue.hh, yiq/get_green.hh,
    	  yuv/get_blue.hh, yuv/get_green.hh: New green and blue getters.
    	* yuv/get_red.hh: New red getter.
    	* yuv/fun.hh: Remove.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1883 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/sandbox/vigouroux/xyz/get_blue.hh
@@ -0,0 +1,56 @@
+#include <cmath>
+
+#include <mln/core/image_if_value.hh>
+#include <mln/core/inplace.hh>
+#include <mln/core/w_window2d_int.hh>
+#include <mln/display/show.hh>
+#include <mln/io/ppm/save.hh>
+#include <mln/display/save_and_show.hh>
+#include <mln/level/fill.hh>
+#include <mln/math/round.hh>
+
+#include "../color/my_xyz.hh"
+
+#ifndef MLN_XYZ_BLUE_HH
+# define MLN_XYZ_BLUE_HH
+
+namespace mln
+{
+
+
+  namespace fun
+  {
+
+    namespace v2v
+    {
+      template <typename T_rgb>
+      struct f_blue_xyz_ : public Function_v2v< f_blue_xyz_<T_rgb> >
+      {
+	typedef T_rgb result;
+
+	template <typename T_xyz>
+	T_rgb operator()(const T_xyz& xyz) const
+	{
+	  typedef typename T_rgb::blue_t   blue_t;
+
+	  static math::round<blue_t>   to_b;
+
+	  blue_t   b = to_b(0.005 * xyz.x() - 0.014 * xyz.y() + 1.01 * xyz.z());
+
+	  T_rgb rgb(0, 0, b);
+
+	  return rgb;
+	}
+      };
+
+      typedef f_blue_xyz_<value::rgb8> f_xyz_blue_t;
+
+      f_xyz_blue_t f_xyz_blue;
+
+    } // end of namespace fun::v2v
+
+  } // end of namespace fun
+
+} // end of namespace mln
+
+#endif // ! MLN_XYZ_BLUE_HH
---
+++ milena/sandbox/vigouroux/yuv/get_blue.hh
@@ -0,0 +1,56 @@
+#include <cmath>
+
+#include <mln/core/image_if_value.hh>
+#include <mln/core/inplace.hh>
+#include <mln/core/w_window2d_int.hh>
+#include <mln/display/show.hh>
+#include <mln/io/ppm/save.hh>
+#include <mln/display/save_and_show.hh>
+#include <mln/level/fill.hh>
+#include <mln/math/round.hh>
+
+#include "../color/my_yuv.hh"
+
+#ifndef MLN_YUV_BLUE_HH
+# define MLN_YUV_BLUE_HH
+
+namespace mln
+{
+
+
+  namespace fun
+  {
+
+    namespace v2v
+    {
+      template <typename T_rgb>
+      struct f_blue_yuv_ : public Function_v2v< f_blue_yuv_<T_rgb> >
+      {
+	typedef T_rgb result;
+
+	template <typename T_yuv>
+	T_rgb operator()(const T_yuv& yuv) const
+	{
+	  typedef typename T_rgb::blue_t   blue_t;
+
+	  static math::round<blue_t>   to_b;
+
+	  blue_t   b = to_b(yuv.y() + 2.03211 * yuv.u());
+
+	  T_rgb rgb(0, 0, b);
+
+	  return rgb;
+	}
+      };
+
+      typedef f_blue_yuv_<value::rgb8> f_yuv_blue_t;
+
+      f_yuv_blue_t f_yuv_blue;
+
+    } // end of namespace fun::v2v
+
+  } // end of namespace fun
+
+} // end of namespace mln
+
+#endif // ! MLN_YUV_BLUE_HH
