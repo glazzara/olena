5cb45953664157ac528dd39929f3b4c38bd9cc8c
Roland Levillain <roland@lrde.epita.fr>
2008-05-05 09:37:51 +0000

    Fix tests/fun/v2v/.
    	* tests/fun/v2v/rgb_hsi_conversion.cc: Add copyright header.
    	Don't use hard-coded paths for I/O operations.
    	* tests/fun/v2v/Makefile.am (check_PROGRAMS): Add
    	rgb_hsi_conversion.
    	(norm_SOURCES): Remove rgb_hsi_conversion.cc.
    	(rgb_hsi_conversion_SOURCES): New.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@1919 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/tests/fun/v2v/rgb_hsi_conversion.cc
+++ milena/tests/fun/v2v/rgb_hsi_conversion.cc
@@ -1,7 +1,37 @@
-#include <mln/value/hsi.hh>
-#include <mln/fun/v2v/rgb_to_hsi.hh>
+// Copyright (C) 2008 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+/// \file tests/fun/v2v/rgb_hsi_conversion.cc
+/// \brief Test RGB-to-HSI conversion.
 
 #include <cmath>
+
+#include <mln/value/hsi.hh>
+#include <mln/fun/v2v/rgb_to_hsi.hh>
 
 #include <mln/core/image2d.hh>
 #include <mln/value/rgb8.hh>
@@ -11,6 +41,7 @@
 #include <mln/math/round.hh>
 #include <mln/level/transform.hh>
 
+#include "tests/data.hh"
 
 
 template <typename I1, typename I2>
@@ -49,7 +80,7 @@ int main()
   using namespace mln;
 
   image2d<value::rgb8> lena;
-  io::ppm::load(lena, "../../img/lena.ppm");
+  io::ppm::load(lena, MLN_IMG_DIR "/lena.ppm");
 
   image2d<value::hsi_f> lena_hsi = level::transform(lena,
 						      fun::v2v::f_rgb_to_hsi_f);
