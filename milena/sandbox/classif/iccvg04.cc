01606e1708017839e471ce6887a96c590cb6f15b
Ugo Jardonnet <jardonnet@lrde.epita.fr>
2008-10-15 09:01:07 +0000

    INIM Classification++.
    	* classif/iccvg04.cc: Revamp + Area Closure.
    	* classif/plotscript: Basic plotting script.
    	* classif/chiche.pgm: Revamp chiche.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/branches/cleanup-2008@2557 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/classif/iccvg04.cc
+++ milena/sandbox/classif/iccvg04.cc
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 #include <mln/core/image/image2d.hh>
@@ -7,6 +6,10 @@
 #include <mln/value/all.hh>
 
 #include <mln/level/fill.hh>
+#include <mln/morpho/closing_area.hh>
+#include <mln/arith/revert.hh>
+#include <mln/morpho/meyer_wst.hh>
+#include <mln/core/alias/neighb2d.hh>
 
 #include <mln/io/ppm/load.hh>
 
@@ -14,12 +17,15 @@
 
 using namespace mln;
 
+unsigned max = 0;
+
 template <typename I>
 mln::image3d<unsigned>
 fill_histo(const I& ima)
 {
-  image3d<unsigned> histo(256,256,256);
+  image3d< unsigned > histo(256,256,256);
   level::fill(histo, 0);
+  unsigned i = 0;
 
   mln_piter(I) p(ima.domain());
   for_all(p)
@@ -30,6 +36,43 @@ fill_histo(const I& ima)
   return histo;
 }
 
+template <typename I>
+void gplot(const I& ima)
+{
+  mln_piter(I) p(ima.domain());
+  for_all(p)
+  {
+    if (ima(p) != 0)
+      std::cout << p[0] << ' ' << p[1] << ' ' << p[2] << std::endl;
+  }
+}
+
+template <typename I>
+void display_proj(const I& histo)
+{
+  image2d< unsigned long long > proj_histo(256,256);
+  level::fill(proj_histo, 0);
+
+  mln_piter_(image2d< unsigned long long >) p(proj_histo.domain());
+  double max = 0;
+  for_all(p)
+  {
+    for (unsigned i = 0; i < 256; i++)
+      proj_histo(p) += histo(point3d(p[0], p[1], i));
+
+    if (proj_histo(p) > max)
+      max = proj_histo(p);
+  }
+
+  image2d< value::int_u8 > out(256, 256);
+  for_all(p)
+  {
+    out(p) = (proj_histo(p) / max) * 255;
+  }
+
+  io::pgm::save(out,"./chiche.pgm");
+}
+
 int main(int argc, char **argv)
 {
   image2d<value::rgb8> ima;
@@ -37,14 +80,12 @@ int main(int argc, char **argv)
 
   image3d<unsigned> histo = fill_histo(ima);
 
-  image2d< value::int_u16 > out(256,256);
-  level::fill(out, 0);
-  mln_piter_(image2d< value::int_u16 >) p(out.domain());
-  for_all(p)
-  {
-    for (unsigned i = 0; i < 256; i++)
-      if ((out(p) + histo(point3d(p[0],p[1],i))) < 65536)
-        out(p) += histo(point3d(p[0],p[1],i));
-  }
-  io::pgm::save(out,"./chiche.pgm");
+  histo = arith::revert(histo);
+  image3d<value::int_u8> histo_closed(histo.domain());
+
+  morpho::closing_area(histo, c4(), 510, histo_closed);
+
+  //display_proj(arith::revert(histo));
+
+  //display_proj(histo);
 }
