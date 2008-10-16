8e7f1df2e8ad1bcbe869a2d3e3695978b6a6b1f0
Roland Levillain <roland@lrde.epita.fr>
2008-10-16 18:50:04 +0000

    Exercise mln::io::load on OFF file (with no color data).
    	* tests/io/Makefile.am (SUBDIRS): Add io.
    	* tests/io/off/: New directory.
    	* tests/io/off/load_bin.cc: New.
    	* tests/io/off/Makefile.am: New.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/branches/cleanup-2008@2586 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/tests/io/off/load_bin.cc
@@ -0,0 +1,64 @@
+// Copyright (C) 2008 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+/// \file tests/io/off/load_bin.cc
+/// \brief Test mln::io::off::load with an
+/// mln::complex_image<3, geom::complex_data<3, point3df>, bool>.
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+#include <mln/io/off/load.hh>
+
+#include "tests/data.hh"
+
+
+int main()
+{
+  using namespace mln;
+
+  /* FIXME: Create an alias in milena/mln/core/alias/ for this
+     particular image type?  */
+  typedef mln::complex_image<2, geom::complex_geometry<2,point3df>, bool> ima_t;
+  ima_t ima;
+  io::off::load(ima, MLN_MESH_DIR "/tetrahedron.off");
+
+  std::cout << ima.domain().cplx() << std::endl;
+
+  mln_piter_(ima_t) p(ima.domain());
+  for_all(p)
+  {
+    std::cout << "ima(" << p << ") = " << ima(p) << "  ( ";
+    // Print site(s).
+    typedef mln_site_(ima_t) site_t;
+    site_t s(p);
+    std::copy (s.sites.begin(), s.sites.end(),
+	       std::ostream_iterator<site_t::location>(std::cout, "  "));
+    std::cout << ")" << std::endl;
+  }
+}
