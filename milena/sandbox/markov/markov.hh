dd7ed4e9aaf803727bc7ca4286cf7736ec19f116
Alexandre Abraham <abraham@lrde.epita.fr>
2008-11-14 11:53:46 +0000

    Add a .cc file, correct some little things.
    	* markov/markov.cc: New.
    	* markov/markov.hh: .
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/branches/cleanup-2008@2877 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/markov/markov.hh
+++ milena/sandbox/markov/markov.hh
@@ -3,69 +3,74 @@
 
 # include <cmath>
 
+namespace mln
+{
 
   inline
-const neighb2d& neighb_h1()
-{
-  static neighb2d it;
-  if (it.size() == 0)
+  const neighb2d& neighb_h1()
   {
-    static const bool vals[] = { 0, 0, 0,
-	                         0, 0, 1,
-                                 0, 0, 0 };
-    convert::from_to(vals, it);
-  }
-  return it;
-}
-
-double compute_energy(const Image2d<int_u8>& ima, bool xi, const point2d& p)
-{
-  // Compute u(x,y)
-  double u;
-  if (v)
-    u = ima(p) / 255;
-  else
-    u = 1 - ima(p) / 255;
-
-  // u(x) is cst donc osef
-
-  // u voisinage
-}
-
-  template <typename I, typename G, typename R, typename E> // I == int_u8
-markov(const Image2d<I>& ima, unsigned start_temp)
-{
-  double espilon = 0.001;
-  Image2d<bool> out(ima.domain()); // FIXME: generalize, we might not do a binarisation
-  G temp(start_temp);
-
-  R v_random(0, 1);
-  R p_random(0., 1.);
-
-  init(ima, out);
-
-  while (temp.value() < epsilon)
-  {
-    qiterator p(ima);
-
-    for_all(p)
-    {
-      bool v = v_random.get();
-
-
-      u = compute_energy(ima, out(p), p);
-      up = compute_energy(ima, v, p);
-
-      d_u = abs(up - u);
-
-      proba = ...;
-
-      if (d_u < 0 or (p_random.get() > proba))
-	out(p) = v;
-    }
+    static neighb2d it;
+    if (it.size() == 0)
+      {
+	static const bool vals[] = { 0, 0, 0,
+				     0, 0, 1,
+				     0, 0, 0 };
+	convert::from_to(vals, it);
+      }
+    return it;
   }
 
-  return out;
-}
+  double compute_energy(const Image2d<int_u8>& ima, bool xi, const point2d& p)
+  {
+    // Compute u(x,y)
+    double u;
+    if (v)
+      u = ima(p) / 255;
+    else
+      u = 1 - ima(p) / 255;
+
+    // u(x) is cst donc osef
+
+    // u voisinage
+  }
+
+  template <typename I, typename G, typename R> // I == int_u8
+  markov(const Image<I>& ima_, unsigned start_temp)
+  {
+    exact(I) &ima = ima_;
+
+    double espilon = 0.001;
+    mln_ch_value(I, bool) out(ima.domain()); // FIXME: generalize, we might not do a binarisation
+    G temp(start_temp);
+
+    R v_random(0, 1);
+    R p_random(0., 1.);
+
+    init(ima, out);
+
+    while (temp.value() < epsilon)
+      {
+	mln_piter p(ima.domain());
+
+	for_all(p)
+	{
+	  bool v = v_random.get();
+
+	  u = compute_energy(ima, out(p), p);
+	  up = compute_energy(ima, v, p);
+
+	  d_u = abs(up - u);
+
+	  proba = ...;
+
+	  if (d_u < 0 or (p_random.get() > proba))
+	    out(p) = v;
+	}
+      }
+
+    return out;
+  }
+
+} // end of namespace mln
 
 #endif /* !MARKOV_HH_ */
