9f4a27c0dc20e88d0a5be29e1f040ee78da98601
Guillaume Lazzara <z@lrde.epita.fr>
2008-11-21 16:06:31 +0000

    Add reference files for example outputs used in the tutorial.
    	* doc/doc.mk: add new path variables.
    
    	* doc/tutorial/figures/ima2d-rot-1.ppm
    	* doc/tutorial/figures/ima2d-rot-2.ppm
    	* doc/tutorial/outputs/borderthickness.txt
    	* doc/tutorial/outputs/dpoint-1.txt
    	* doc/tutorial/outputs/graph-data-full.txt
    	* doc/tutorial/outputs/ima-has.txt
    	* doc/tutorial/outputs/ima-size.txt
    	* doc/tutorial/outputs/ima2d-1.txt
    	* doc/tutorial/outputs/ima2d-decl-2-blobs-full.txt
    	* doc/tutorial/outputs/ima2d-rot.txt
    	* doc/tutorial/outputs/labeling-compute-full.txt
    	* doc/tutorial/outputs/logical-not.txt
    	* doc/tutorial/outputs/paste-call-1.txt
    	* doc/tutorial/outputs/point-1.txt: reference files.
    
    	* doc/tutorial/samples/Makefile.am: diff the generated files to their
    	reference.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@2941 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/doc/doc.mk
+++ milena/doc/doc.mk
@@ -2,6 +2,12 @@
 
 include $(top_srcdir)/milena/tests/tests.mk
 
-OUTPUTS_DIR = $(top_builddir)/milena/doc/tutorial/outputs
-FIGURES_DIR = $(top_builddir)/milena/doc/tutorial/figures
+TUTORIAL_SRCDIR = $(top_srcdir)/milena/doc/tutorial
+OUTPUTS_SRCDIR = $(TUTORIAL_SRCDIR)/outputs
+FIGURES_SRCDIR = $(TUTORIAL_SRCDIR)/figures
+
+TUTORIAL_BUILDDIR = $(top_builddir)/milena/doc/tutorial
+OUTPUTS_BUILDDIR = $(TUTORIAL_BUILDDIR)/outputs
+FIGURES_BUILDDIR = $(TUTORIAL_BUILDDIR)/figures
+
 
