4ec694d92fc4eaedc94e5fed0abf75b48316d9ce
Thierry Géraud <theo@lrde.epita.fr>
2009-01-20 13:33:46 +0000

    Augment Laurent's ISMM code.
    	* laurent/playing_with_attributes.cc: Augment.
    	* laurent/ismm2009.cc: Memorize as...
    	* laurent/ismm2009.v2.cc: ...this new file.
    	* laurent/ismm2009.cc: Augment.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3173 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/laurent/playing_with_attributes.cc
+++ milena/sandbox/laurent/playing_with_attributes.cc
@@ -13,6 +13,10 @@
 #include <mln/morpho/tree/compute_attribute_image.hh>
 #include <mln/labeling/regional_minima.hh>
 
+#include <mln/core/site_set/p_array.hh>
+#include <mln/level/sort_psites.hh>
+#include <mln/geom/nsites.hh>
+
 #include <mln/accu/count.hh>
 
 
@@ -26,6 +30,200 @@ void usage(char* argv[])
 
 
 
+namespace mln
+{
+
+
+  template <typename T>
+  struct node_pred : Function_p2b< node_pred<T> >
+  {
+    typedef bool result;
+
+    template <typename P>
+    bool operator()(const P& p) const
+    {
+      return t->is_a_node(p);
+    }
+
+    const T* t;
+  };
+
+
+
+  template <typename T, typename I>
+  void depict_tree_attributes(const T& t, // Tree.
+			      const I& a) // Attribute image.
+  {
+    typedef mln_site(I) P;
+
+    mln_ch_value(I, bool) deja_vu;
+    initialize(deja_vu, a);
+    data::fill(deja_vu, false);
+
+    typedef typename T::nodes_t nodes_t;
+    mln_fwd_piter(nodes_t) p(t.nodes());
+    for_all(p)
+      {
+	if (deja_vu(p))
+	  continue;
+	
+	P e = p;
+	do
+	  {
+	    std::cout << a(e) << ':' << e << "  ->  ";
+	    deja_vu(e) = true;
+	    e = t.parent(e);
+	  }
+	while (! deja_vu(e));
+	std::cout << a(e) << ':' << e << std::endl;
+      }
+    std::cout << std::endl;
+  }
+
+
+  template <typename T, typename I>
+  mln_concrete(I)
+    change_tree_attributes(const T& t, // Tree.
+			   const I& a, // Attribute image.
+			   bool echo = false)
+  {
+    typedef mln_site(I) P;
+    typedef mln_value(I) A; // Type of attributes.
+
+    // Test that attributes increase with parenthood.
+    mln_fwd_piter(T) p(t.domain());
+    for_all(p)
+      if (t.is_a_non_root_node(p))
+	{
+	  mln_assertion(t.is_a_node(t.parent(p)));
+	  mln_invariant(a(t.parent(p)) >= a(p));
+	}
+
+
+    if (echo)
+      depict_tree_attributes(t, a);
+
+    
+    node_pred<T> node_only;
+    node_only.t = &t;
+
+    typedef p_array<P> S;
+    S s = level::sort_psites_increasing(a | node_only);
+    mln_invariant(geom::nsites(a | t.nodes()) == s.nsites());
+
+
+    mln_ch_value(I, bool) mark;
+    initialize(mark, a);
+    data::fill(mark, false);
+
+
+    // Modify attributes.
+
+    mln_concrete(I) aa;
+    initialize(aa, a);
+    data::fill(aa, 0);
+
+    {
+
+      P e, par_e;
+      A v;
+      bool found;
+
+      mln_fwd_piter(S) p(s);
+      
+      // Initialization.
+      for_all(p)
+	{
+	  if (mark(p) == true)
+	    continue;
+
+	  // First, fetch the attribute value v from p;
+	  e = p;
+	  found = false;
+	  while (mark(e) == false && e != t.parent(e))
+	    {
+	      par_e = t.parent(e);
+	      if (mark(par_e) == true)
+		{
+		  v = a(e);
+		  found = true;
+		  break;
+		}
+	      e = par_e;
+	    };
+
+	  if (! found && e == t.parent(e))
+	    {
+	      // e is a root node; we get its value:
+	      v = a(e);
+
+	      if (echo)
+		std::cout << "from p = " << p
+			  << "  to root e = " << e
+			  << "  v = " << v
+			  << std::endl;
+
+	      // we set aa from p to the root node
+	      e = p;
+	      do
+		{
+		  aa(e) = v;
+		  mln_invariant(mark(e) == false);
+		  mark(e) = true;
+		  e = t.parent(e);
+		}
+	      while (e != t.parent(e));
+	      // handle the root node 'e':
+	      aa(e) = v;
+	      mln_invariant(mark(e) == false);
+	      mark(e) = true;
+	      mln_invariant(a(e) == v);
+	    }
+	  else if (found)
+	    {
+	      e = p;
+
+	      P last_e = P(0,0);
+
+	      while (mark(e) == false)
+		{
+		  aa(e) = v;
+		  mark(e) = true;
+		  last_e = e;
+		  e = t.parent(e);
+		};
+
+	      if (echo)
+		{
+		  if (last_e == p)
+		    std::cout << "p = " << p
+			      << "  par(e) = " << par_e
+			      << "  so keep v = " << v
+			      << std::endl;
+		  else
+		    std::cout << "from p = " << p
+			      << "  to e = " << last_e
+			      << "  with par(e) = " << par_e
+			      << "  v = " << v
+			      << std::endl;
+		}
+	    }
+	}
+
+      if (echo)
+	depict_tree_attributes(t, aa);
+
+    } // end of Modify attributes.
+
+    return aa;
+  }
+
+
+
+} // mln
+
+
+
 template <typename I, typename N, typename L>
 void do_it(const I& g, const N& nbh, L& n_labels)
 {
@@ -34,7 +232,7 @@ void do_it(const I& g, const N& nbh, L& n_labels)
   // regional minima
 
   mln_ch_value(I,L) regmin = labeling::regional_minima(g, nbh, n_labels);
-  debug::println("regmin(g):", regmin);
+  // debug::println("regmin(g):", regmin);
 
 
   // watershed
@@ -42,7 +240,7 @@ void do_it(const I& g, const N& nbh, L& n_labels)
   L n_basins;
   mln_ch_value(I,L) w = morpho::meyer_wst(g, nbh, n_basins);
   mln_invariant(n_basins == n_labels);
-  debug::println("w(g):", w);
+  // debug::println("w(g):", w);
 
 
   {
@@ -55,12 +253,28 @@ void do_it(const I& g, const N& nbh, L& n_labels)
     accu::count< util::pix<I> > a_; // Kind of attribute.
  
     mln_ch_value(I,unsigned) a = morpho::tree::compute_attribute_image(a_, t);
-    debug::println("a:", a);
+    debug::println("a | nodes:", a | t.nodes());
+
+    mln_ch_value(I,unsigned) aa = change_tree_attributes(t, a);
+    debug::println("aa | nodes:", aa | t.nodes());
+
+    // Back-propagation from nodes to components.
+    {
+      mln_fwd_piter(tree_t) p(t.domain());
+      for_all(p)
+	if (! t.is_a_node(p))
+	  {
+	    mln_assertion(t.is_a_node(t.parent(p)));
+	    aa(p) = aa(t.parent(p));
+	  }
+    }
 
     debug::println("a | w line:", a | (pw::value(w) == pw::cst(0)));
-    debug::println("a | w basins:", a | (pw::value(w) != pw::cst(0)));
+    debug::println("aa | w line:", aa | (pw::value(w) == pw::cst(0)));
 
-    debug::println("a | regmin:", a | (pw::value(regmin) != pw::cst(0)));
+//     debug::println("a | w basins:", a | (pw::value(w) != pw::cst(0)));
+//     debug::println("a | regmin:", a | (pw::value(regmin) != pw::cst(0)));
+
   }
 
 } // end of do_it
