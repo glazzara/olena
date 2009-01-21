c30315b243836cd6014f30d1d80062bfc3dc2899
Alexandre Abraham <abraham@lrde.epita.fr>
2009-01-21 14:36:43 +0000

    Fix call to 'at'.
    	* abraham/mln/transform/fft.hh: Add forgotten opt:: before at.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3183 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/abraham/mln/transform/fft.hh
+++ milena/sandbox/abraham/mln/transform/fft.hh
@@ -1,4 +1,4 @@
-// Copyright (C) 2007, 2008 EPITA Research and Development Laboratory
+// Copyright (C) 2007, 2008, 2009 EPITA Research and Development Laboratory
 //
 // This file is part of the Olena Library.  This library is free
 // software; you can redistribute it and/or modify it under the terms
@@ -442,7 +442,7 @@ namespace mln {
 	    }
 	for (unsigned row = 0; row < this->trans_im.nrows(); ++row)
 	  for (unsigned col = this->trans_im.ncols() - 1; col > this->trans_im.ncols() / 2; --col)
-	    at(this->trans_im, row, col) = opt::at(this->trans_im, this->trans_im.nrows() - row - 1,
+	    opt::at(this->trans_im, row, col) = opt::at(this->trans_im, this->trans_im.nrows() - row - 1,
 							    this->trans_im.ncols() - col - 1);
 	return this->trans_im;
       }
