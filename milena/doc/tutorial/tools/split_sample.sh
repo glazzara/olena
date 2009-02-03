6dc1fbc9b49be6ebb2328bc59835ef8f85aeb784
Roland Levillain <roland@lrde.epita.fr>
2009-02-03 18:41:36 +0000

    Typo: s/splitted/split/.
    	* milena/doc/Doxyfile.in,
    	* milena/doc/doc.mk,
    	* milena/doc/tutorial/tools/split_sample.sh,
    	* milena/doc/tutorial/tutorial.tex: Here.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3275 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/doc/tutorial/tools/split_sample.sh
+++ milena/doc/tutorial/tools/split_sample.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+# /bin/sh
 
 if ! [ $# -eq 3 ]; then
   echo "Usage: $0 <file1> <file extension> <dst dir>"
@@ -8,16 +8,15 @@ fi
 begins=`grep -n -R '//[[:space:]]*\\\{' $1 | cut -d ':' -f 1`
 ends=`grep -n -R '//[[:space:]]*\\\}' $1 | cut -d ':' -f 1`
 
-splitted=false
+split=false
 i=1
 for begin in $begins; do
   partend=$((`echo $ends | cut -d ' ' -f $i` - 1))
   head -n $partend $1 | tail -n $(($partend - $begin)) > $3/"`basename $1 .$2`-$i.$2"
   i=$((i+1))
-  splitted=true
+  split=true
 done
 
-if [ $splitted ] && [ $i -gt 1 ]; then
-  echo "* $1 has been splitted in $i files."
+if [ $split ] && [ $i -gt 1 ]; then
+  echo "* $1 has been split in $i files."
 fi
-
