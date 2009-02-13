acb11006022fcae6e0e52069a0d957d18d4cda38
Thierry Géraud <theo@lrde.epita.fr>
2009-02-13 13:55:06 +0000

    Add source for command executables.
    	* theo/igr/dump2pgm.cc: Move and rename as...
    	* theo/exec/dump2pnm.cc: ...this.
    	Update to also handle pbm and ppm.
    	* theo/igr/pgms2dump.cc: Move and rename as...
    	* theo/exec/pnms2dump.cc: ...this.
    	Update to also handle pbm and ppm.
    	* theo/exec/clean_holes.cc: New.
    	* theo/exec/threshold_low.cc: New.
    	* theo/exec/dump2cloud.cc: New.
    	* theo/exec/closing_area.cc: New.
    	* theo/exec/closing_isotropic.cc: New.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3372 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/sandbox/theo/exec/closing_isotropic.cc
@@ -0,0 +1,71 @@
+#include "filetype.hh"
+
+#include <mln/morpho/closing.hh>
+#include <mln/win/disk2d.hh>
+#include <mln/win/ball3d.hh>
+
+
+
+void usage(char* argv[])
+{
+  std::cerr << "usage: " << argv[0] << " input.xxx r output.xxx" << std::endl
+	    << "  Closing with an isotropic structuring element." << std::endl
+	    << "  xxx is pbm (2D) or dump (3D)." << std::endl;
+  std::abort();
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  using namespace mln;
+
+  if (argc != 4)
+    usage(argv);
+
+  trace::entering("main");
+
+  int r = std::atoi(argv[2]);
+  if (r < 0)
+    {
+      std::cerr << "bad radius!" << std::endl;
+      usage(argv);
+    }
+
+
+  std::string filename = argv[1];
+
+  switch (get_filetype(argv[1]))
+    {
+    case filetype::pbm:
+      {
+	image2d<bool> ima, out;
+	io::pbm::load(ima, argv[1]);
+	out = morpho::closing(ima, win::disk2d(2 * r + 1));
+	io::pbm::save(out, argv[3]);
+      }
+      break;
+
+    case filetype::dump:
+      {
+	image3d<bool> ima, out;
+	io::dump::load(ima, argv[1]);
+	out = morpho::closing(ima, win::ball3d(2 * r + 1));
+	io::dump::save(out, argv[3]);
+      }
+      break;
+
+    case filetype::unknown:
+      std::cerr << "unknown filename extension!" << std::endl;
+      usage(argv);
+      break;
+
+    default:
+      std::cerr << "file type not handled!" << std::endl;
+      usage(argv);
+    }
+
+
+
+  trace::exiting("main");
+}
