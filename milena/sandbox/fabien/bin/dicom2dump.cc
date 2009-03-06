e99ee6609ee7243b106a7713dd079e224c4050e0
Fabien Freling <freling@lrde.epita.fr>
2009-03-06 17:00:18 +0000

    Add watershed implementation for IRM images.
    	* fabien/bin/Makefile: Update.
    	* fabien/bin/dicom2dump.cc: Update.
    	* fabien/bin/dicom_mask.cc: Update.
    	* fabien/igr/Makefile: Update.
    	* fabien/igr/dumps: Remove.
    	* fabien/igr/watershed.cc: Watershed implementation.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3496 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/fabien/bin/dicom2dump.cc
+++ milena/sandbox/fabien/bin/dicom2dump.cc
@@ -2,7 +2,6 @@
 #include <mln/core/image/image2d.hh>
 #include <mln/core/image/image3d.hh>
 
-#include <mln/value/int_u8.hh>
 #include <mln/value/int_u12.hh>
 #include <mln/io/dicom/load.hh>
 #include <mln/io/dump/save.hh>
@@ -17,7 +16,7 @@ int usage(char* argv[])
 }
 
 
-/*int main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
   using namespace mln;
   using value::int_u12;
@@ -26,22 +25,6 @@ int usage(char* argv[])
     return usage(argv);
 
   image3d<int_u12> ima;
-  io::dicom::load(ima, argv[1]);
-  io::dump::save(ima, argv[2]);
-
-  return 0;
-}*/
-
-int main(int argc, char* argv[])
-{
-  using namespace mln;
-  using value::int_u8;
-  using value::int_u12;
-
-  if (argc != 3)
-    return usage(argv);
-
-  image2d<int_u8> ima;
   io::dicom::load(ima, argv[1]);
   io::dump::save(ima, argv[2]);
 
--- milena/sandbox/fabien/bin/dicom_mask.cc
+++ milena/sandbox/fabien/bin/dicom_mask.cc
@@ -1,6 +1,9 @@
 #include <mln/core/concept/image.hh>
 #include <mln/core/image/image2d.hh>
 
+#include <mln/geom/ncols.hh>
+#include <mln/geom/nrows.hh>
+
 #include <mln/value/int_u8.hh>
 #include <mln/io/dicom/load.hh>
 #include <mln/io/pbm/save.hh>
@@ -32,6 +35,29 @@ int main(int argc, char* argv[])
 
   image2d<int_u8> input;
   io::dicom::load(input, argv[1]);
+
+  util::array<unsigned> xproj(geom::nrows(input), 0);
+  util::array<unsigned> yproj(geom::ncols(input), 0);
+
+  mln_piter_(image2d<int_u8>) p(input.domain());
+  for_all(p)
+  {
+    xproj[p.row()] += input(p);
+    yproj[p.col()] += input(p);
+  }
+
+  // Plot files
+  std::ofstream fout_x("x.plot");
+  for (unsigned int i = 0; i < xproj.nelements(); ++i)
+  {
+    fout_x << i << " " << xproj[i] << std::endl;
+  }
+
+  std::ofstream fout_y("y.plot");
+  for (unsigned int i = 0; i < yproj.nelements(); ++i)
+  {
+    fout_y << i << " " << yproj[i] << std::endl;
+  }
 
   image2d<bool> ima = level::transform(input, fun::v2b::threshold<int_u8>(1));
   io::pbm::save(ima, argv[2]);
