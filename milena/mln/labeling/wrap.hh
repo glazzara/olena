5be43fc7167eb29ee6332da856532c627a428209
Guillaume Lazzara <z@lrde.epita.fr>
2009-04-02 07:10:51 +0000

    Add labeling::wrap routine.
    	* mln/labeling/wrap.hh: new routine.
    
    	* tests/labeling/Makefile.am,
    	* tests/labeling/wrap.cc: new test.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3601 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ milena/mln/labeling/wrap.hh
@@ -0,0 +1,86 @@
+// Copyright (C) 2009 EPITA Research and Development Laboratory
+// (LRDE)
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+#ifndef MLN_LABELING_WRAP_HH
+# define MLN_LABELING_WRAP_HH
+
+/// \file mln/labeling/wrap.hh
+///
+/// Wrap labels such as 0 -> 0 and [1, lmax] maps to [1,
+/// Lmax] (using modulus).
+
+# include <mln/core/concept/image.hh>
+# include <mln/level/transform.hh>
+# include <mln/fun/l2l/wrap.hh>
+# include <mln/metal/converts_to.hh>
+# include <mln/metal/is_a.hh>
+
+namespace mln
+{
+
+  namespace labeling
+  {
+
+    /// Wrap labels such as 0 -> 0 and [1, lmax] maps to [1,
+    /// Lmax] (using modulus).
+    ///
+    /// \param[in] value_type The type used to wrap the label type.
+    /// \param[in] input The label image.
+    ///
+    /// \return A new image with values wrapped with type V.
+    template <typename V, typename I>
+    mln_ch_value(I,V)
+    wrap(const V& value_type, const Image<I>& input);
+
+
+
+# ifndef MLN_INCLUDE_ONLY
+
+    template <typename V, typename I>
+    inline
+    mln_ch_value(I,V)
+    wrap(const V& value_type, const Image<I>& input)
+    {
+      trace::entering("labeling::wrap");
+
+//      mlc_is_a(mln_value(I), value::Symbolic)::check();
+      mln_precondition(exact(input).is_valid());
+
+      mln_ch_value(I,V) output = level::transform(input, fun::l2l::wrap<V>());
+
+      trace::exiting("labeling::wrap");
+      return output;
+    }
+
+# endif // ! MLN_INCLUDE_ONLY
+
+  } // end of namespace mln::labeling
+
+} // end of namespace mln
+
+#endif // ! MLN_LABELING_WRAP_HH
---
+++ milena/tests/labeling/wrap.cc
@@ -0,0 +1,64 @@
+// Copyright (C) 2009 EPITA Research and Development Laboratory
+// (LRDE)
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+/// \file tests/labeling/wrap.cc
+///
+/// Test on mln::labeling::wrap.
+
+#include <mln/core/image/image2d.hh>
+#include <mln/core/alias/neighb2d.hh>
+#include <mln/value/int_u8.hh>
+#include <mln/value/int_u16.hh>
+#include <mln/make/image2d.hh>
+#include <mln/labeling/wrap.hh>
+#include <mln/level/compare.hh>
+
+#include <mln/debug/println.hh>
+
+#include "tests/data.hh"
+
+
+int main()
+{
+  using namespace mln;
+  using value::int_u8;
+  using value::int_u16;
+
+  int_u16 values[] = { 1, 1000,
+		       0, 65534 };
+
+  int_u8 ref_values[] = { 1, 235,
+			  0, 254 };
+
+  image2d<int_u16> ima = make::image2d(values);
+  image2d<int_u8> ima_ref = make::image2d(ref_values);
+
+  image2d<int_u8> ima_wrapped = labeling::wrap(int_u8(), ima);
+
+  mln_assertion(ima_ref == ima_wrapped);
+}
