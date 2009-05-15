f8cea2b933f93e0c9a32a1f5389a0d0f11c73042
Étienne Folio <folio@lrde.epita.fr>
2009-05-15 20:55:00 +0000

    Big cleanup.
    	* Makefile: Remove. Not needed.
    	* README: New explainations.
    	* configure: To build the Makefile.
    	* dilation-n.hh: New algorithm.
    	* erosion-n.hh: New algorithm.
    	* main_dilation-n.cc: New test file.
    	* morpho.cc: Cleanup in here.
    	* treat.sh: Adaptation.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3846 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/inim/2010/morpho/morpho.cc
+++ milena/sandbox/inim/2010/morpho/morpho.cc
@@ -1,171 +1,144 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <mln/debug/println.hh>
 
+#include <mln/arith/revert.hh>
+#include <mln/core/alias/neighb2d.hh>
+#include <mln/core/alias/window2d.hh>
 #include <mln/io/pbm/load.hh>
 #include <mln/io/pgm/save.hh>
-
-
-#include <mln/core/alias/neighb2d.hh>
-#include <mln/accu/max.hh>
-#include <mln/value/label_8.hh>
 #include <mln/labeling/regional_maxima.hh>
-#include <mln/data/fill.hh>
-#include <mln/literal/all.hh>
-#include <mln/arith/revert.hh>
 #include <mln/level/convert.hh>
 #include <mln/level/stretch.hh>
-#include <mln/morpho/closing/structural.hh>
-#include <mln/core/alias/window2d.hh>
-#include <mln/make/win_chamfer.hh>
 #include <mln/linear/gaussian.hh>
-
 #include <mln/morpho/rank_filter.hh>
+#include <mln/value/label_8.hh>
 
 using namespace mln;
 using namespace mln::value;
 
 void
-load_img(image2d<bool>& in, const char* filename)
+load_img(image2d<int_u8>& ima, const char* filename)
 {
-  using namespace mln;
-
-  // build test image
-  std::cout << "  => loading " << filename << "...";
+  std::cout << "  => loadimag " << filename << "...";
   std::cout.flush();
+
+  // load.
+  image2d<bool> in;
   io::pbm::load(in, filename);
+
+  // convert and stretch.
+  int_u8 k = 1;
+  ima = level::convert(k, in);
+  ima = level::stretch(k, ima);
+
   std::cout << "................[DONE]" << std::endl;
 }
 
-
 void
-revert_img(image2d<int_u8>& out,
-	   const image2d<bool>& in)
+revert_img(image2d<int_u8>& ima)
 {
-  using namespace mln;
-
-  int_u8 k = 1;
-  out = level::convert(k, in);
-  out = level::stretch(k, out);
-
   std::cout << "  => revert image...";
   std::cout.flush();
-  out = arith::revert(out);
-  std::cout << "...................[DONE]" << std::endl;
 
+  // revert.
+  ima = arith::revert(ima);
+  std::cout << "...................[DONE]" << std::endl;
 }
 
-
-
-
 void
-gaussian_filter(image2d<int_u8>& out, int gaussian_force)
+gaussian_filter(image2d<int_u8>& ima, float gaussian_force)
 {
-  using namespace mln;
-
   std::cout << "  => applying gaussian filter...";
   std::cout.flush();
-  out = linear::gaussian(out, gaussian_force, 1);
-  //  out =  linear::gaussian_1st_derivative(out, gaussian_force, 1);
+
+  // gaussian filter.
+  ima = linear::gaussian(ima, gaussian_force, 1);
   std::cout << ".......[DONE]" << std::endl;
 }
 
-
-
-
-
-
-image2d<int_u8>
-rank_filter(const image2d<int_u8>& out,
-	    window2d it, int k, unsigned int lambda)
+void
+rank_filter(image2d<int_u8>& ima,
+	    const window2d& it,
+	    const unsigned k,
+	    const unsigned lambda)
 {
-  using namespace mln;
-
   std::cout << "  => applying rank k filter...";
   std::cout.flush();
-  image2d<int_u8> rank_filtered = morpho::rank_filter(out, it, k);
 
+  // rank filter.
+  ima = morpho::rank_filter(ima, it, k);
 
-  for (unsigned int y = 0; y <= rank_filtered.ncols () - 1; ++y)
-    for (unsigned  int x = 0; x <= rank_filtered.nrows() - 1; ++x)
-      {
-	if (rank_filtered(point2d(x, y)) < lambda)
- 	  rank_filtered(point2d(x, y)) = 0;
-	else
- 	  rank_filtered(point2d(x, y)) = 255;
-      }
+  mln_fwd_piter_(image2d<int_u8>) p(ima.domain());
+  for_all(p)
+    if (ima(p) < lambda)
+      ima(p) = mln_min(int_u8);
+    else
+      ima(p) = mln_max(int_u8);
 
   std::cout << ".........[DONE]" << std::endl;
-
-  return rank_filtered;
 }
 
-
-
-
-
 void
-look_for_lines(const image2d<label_8>& labels, image2d<int_u8>& ima)
+replace_with_labels(const image2d<label_8>& labels, image2d<int_u8>& ima)
 {
   mln_fwd_piter_(image2d<int_u8>) p(ima.domain());
   for_all(p)
-  {
     if (ima(p) == mln_max(int_u8))
-    {
-      ima(p) = labels(p);
-    }
-  }
+      ima(p) = labels(p); // replace with label at p.
 }
-
 
 int
 main(int argc, char** argv)
 {
   // check arguments
   if (argc != 7)
-    {
-      std::cerr << "Usage:" << std::endl
-		<< "  ./morpho <in.pbm> <gaussian_force> <k> "
-		<< "<window_width> <lambda> "
-		<< "<output_filename>" << std::endl;
-      exit(1);
-    }
+  {
+    std::cerr << "Usage:" << std::endl
+	      << "  ./morpho <ima.pbm> <gaussian_force> <k> "
+	      << "<window_width> <lambda> "
+	      << "<input_filename>" << std::endl;
+    exit(1);
+  }
 
+  // get argv variables.
   const char* input_filename = argv[1];
-  float gaussian_force = atof(argv[2]);
-
-  unsigned int k = atoi(argv[3]);
-  unsigned int w_width = atoi(argv[4]);
-  unsigned int lambda = atoi(argv[5]);
+  const float gaussian_force = atof(argv[2]);
+  const unsigned k = atoi(argv[3]);
+  const unsigned w_width = atoi(argv[4]);
+  const unsigned lambda = atoi(argv[5]);
   const char* output_filename = argv[6];
 
+  // load and revert image.
+  image2d<int_u8> ima;
+  load_img(ima, input_filename);
+  image2d<int_u8> in(ima);
+  //revert_img(ima);
+  io::pgm::save(ima, "1.pgm");
 
-  image2d<bool> in;
-  load_img(in, input_filename);
+  // apply gaussian filter.
+  gaussian_filter(ima, gaussian_force);
+  io::pgm::save(ima, "2.pgm");
 
-  int_u8 i = 1;
-  image2d<int_u8> in_u8 = level::convert(i, in);
-  in_u8 = level::stretch(i, in_u8);
-
-  image2d<int_u8> out = in_u8;
-  gaussian_filter(out, gaussian_force);
-
+  // create window.
   window2d window;
   window.insert(0, 0);
-
   for (unsigned i = 1; i <= w_width; ++i)
   {
     window.insert(0, -i);
     window.insert(0, i);
   }
 
-  image2d<int_u8> ima =
-    rank_filter(out, window, k, lambda);
+  // apply k-rank filter.
+  rank_filter(ima, window, k, lambda);
+  io::pgm::save(ima, "3.pgm");
 
+  // labelize.
   label_8 nlabels = 0;
   image2d<label_8> labels = labeling::regional_maxima(ima, c4(), nlabels);
+  io::pgm::save(labels, "4.pgm");
 
-  look_for_lines(labels, in_u8);
-  io::pgm::save(in_u8, output_filename);
+  // compute output image.
+  replace_with_labels(labels, in);
+  io::pgm::save(in, output_filename);
 }
--- milena/sandbox/inim/2010/morpho/treat.sh
+++ milena/sandbox/inim/2010/morpho/treat.sh
@@ -1,42 +1,21 @@
 #!/bin/sh
 
-
-
 gaussian_force=20      # best with values around 20
 k=5                    # best with values in [|1..10|]
 window_width=20        # best with small values in [|10..30|]
 lambda=5               # best with small values like 5-10
-OUTPUT_DIR=output
 
-
-if [ $# -eq 0 ]
+if [ $# -ne 2 ]
 then
-
-    echo -n 'Cleaning output directory........'
-    rm -f /lrde/beyrouth/lrde-2011/da-mota/images_INIM/result/*
-    echo '......[DONE]'
-
-    mkdir -p ${OUTPUT_DIR}
-
-    for filename in `echo *.pbm`
-    do
-	output_filename=${OUTPUT_DIR}/${filename}
-
-	~/morpho/morpho           \
-	    ${filename}           \
-	    ${gaussian_force}     \
-	    ${k}                  \
-	    ${window_width}       \
-	    ${lambda}             \
-	    ${output_filename}    \
-	|| exit $?
-
-    done
-
+    echo "Usage: ./run.sh <input_image.pbm> <output_image.pgm>"
+    exit 1
 fi
 
-
-for i in `ls ${OUTPUT_DIR}/*.pbm`
-do
-    display $i
-done
+./morpho                  \
+    $1                    \
+    ${gaussian_force}     \
+    ${k}                  \
+    ${window_width}       \
+    ${lambda}             \
+    $2                    \
+    || exit $?
