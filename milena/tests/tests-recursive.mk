279b14d15156a23d986449f27ba0732a156e9fe3
Roland Levillain <roland@lrde.epita.fr>
2009-06-03 17:29:43 +0000

    Normalize Makefiles' (short) headers.
    	* tests/accu/stat/Makefile.am,
    	* tests/fun/stat/Makefile.am,
    	* tests/linear/gaussian/Makefile.am,
    	* tests/morpho/reconstruction/Makefile.am,
    	* tests/morpho/reconstruction/by_dilation/Makefile.am,
    	* tests/morpho/reconstruction/by_erosion/Makefile.am,
    	* tests/topo/skeleton/Makefile.am,
    	* tests/transformation/Makefile.am,
    	* tests/world/binary_2d/Makefile.am,
    	* tests/world/inter_pixel/Makefile.am:
    	Do not impose Emacs' Makefile major mode.
    	* tests/tests-recursive.mk: Typo in comment.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@3979 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/tests/tests-recursive.mk
+++ milena/tests/tests-recursive.mk
@@ -1,5 +1,5 @@
 # tests-recursive.mk					-*- Automake -*-
-# Compile tests recursively, but don't them.
+# Compile tests recursively, but don't run them.
 
 # Copyright (C) 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
 # 2003, 2004, 2005, 2006, 2008  Free Software Foundation, Inc.
