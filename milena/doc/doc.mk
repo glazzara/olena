a0fdae30bf5f9686c1e52857b07cb693955ebe3f
Roland Levillain <roland@lrde.epita.fr>
2009-06-26 14:33:40 +0000

    Generate the documentation in the source tree.
    	* doc/Makefile.am
    	(EXTRA_DEPS)
    	(USER_REFMAN_PDF, USER_REFMAN_HTML)
    	(DEVEL_REFMAN_PDF, DEVEL_REFMAN_HTML):
    	Prepend $(srcdir) to each path.
    	($(srcdir)/tutorial/tutorial.hh, $(srcdir)/ref_guide/ref_guide.hh)
    	($(USER_REFMAN_PDF), $(USER_REFMAN_HTML))
    	($(DEVEL_REFMAN_PDF), $(DEVEL_REFMAN_HTML))
    	($(srcdir)/figures.stamp, fake-doc)
    	Adjust actions.
    	(edit): Substitute @srcdir@ and @builddir@ as well.
    	(edit_user): Prepend $(srcdir) to each path.
    	(Doxyfile_user, Doxyfile_user_pdf)
    	(Doxyfile_devel, Doxyfile_devel_pdf):
    	Rename targets as...
    	($(srcdir)/Doxyfile_user, $(srcdir)/Doxyfile_user_pdf)
    	($(srcdir)/Doxyfile_devel, $(srcdir)/Doxyfile_devel_pdf):
    	...these.
    	* doc/doc.mk (DOC_BUILDDIR, OUTPUTS_BUILDDIR)
    	(SPLIT_OUTPUTS_BUILDDIR, FIGURES_BUILDDIR, EXAMPLES_BUILDDIR)
    	(SPLIT_EXAMPLES_BUILDDIR):
    	Remove variables.
    	Add missing copyright header.
    	* doc/Doxyfile.in (OUTPUT_DIRECTORY): Set to @srcdir@/devel/.
    	* doc/examples/Makefile.am (CLEANFILES): New.
    	Add $(EXTRA_PROGRAMS)
    	(run-examples, diff-data, split-examples, split-outputs):
    	Adjust actions to generate products in the source tree.
    	(clean-local, clean-figures): Disable actions.
    	* doc/ref_guide/Makefile.am (TEXINPUTS): Remove $(DOC_BUILDDIR).
    	($(REF_GUIDE_HH)): Adjust the action to generate the target in the
    	source tree.
    	* doc/tutorial/Makefile.am (TEXINPUTS): Remove $(DOC_BUILDDIR).
    	($(TUTORIAL_HH)): Adjust the action to generate the target in the
    	source tree.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4204 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/doc/doc.mk
+++ milena/doc/doc.mk
@@ -1,10 +1,31 @@
-## FIXME: Revamp (see Olena 0.11's test suite).		-*- Automake -*-
+#								-*- Automake -*-
+
+# Copyright (C) 2008, 2009 EPITA Research and Development Laboratory (LRDE).
+#
+# This file is part of Olena.
+#
+# Olena is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2 of the License.
+#
+# Olena is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Olena.  If not, see <http://www.gnu.org/licenses/>.
 
 include $(top_srcdir)/milena/tests/tests.mk
 
 AM_CPPFLAGS += -I$(top_srcdir)/milena/doc/tutorial/tools
 
-
+## FIXME: Upercase variables should be reserved for special names:
+## classical Make variables (e.g. `CXXFLAGS') Automake variables
+## (e.g. `SUBDIRS'), etc.
+##
+## Moreover, do we really need all these variables?  See how things
+## evolve during the refurbishing of the build system in doc/.
 DOC_SRCDIR = $(top_srcdir)/milena/doc
 OUTPUTS_SRCDIR = $(DOC_SRCDIR)/outputs
 SPLIT_OUTPUTS_SRCDIR = $(OUTPUTS_SRCDIR)/split
@@ -12,10 +33,3 @@ FIGURES_SRCDIR = $(DOC_SRCDIR)/figures
 EXAMPLES_SRCDIR = $(DOC_SRCDIR)/examples
 SPLIT_EXAMPLES_SRCDIR = $(EXAMPLES_SRCDIR)/split
 IMG_SRCDIR = $(DOC_SRCDIR)/img
-
-DOC_BUILDDIR = $(top_builddir)/milena/doc
-OUTPUTS_BUILDDIR = $(DOC_BUILDDIR)/outputs
-SPLIT_OUTPUTS_BUILDDIR = $(OUTPUTS_BUILDDIR)/split
-FIGURES_BUILDDIR = $(DOC_BUILDDIR)/figures
-EXAMPLES_BUILDDIR = $(DOC_BUILDDIR)/examples
-SPLIT_EXAMPLES_BUILDDIR = $(EXAMPLES_BUILDDIR)/split
