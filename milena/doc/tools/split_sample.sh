95ca51bf0334cdf0240994f60e4139825375e56c
Guillaume Lazzara <z@lrde.epita.fr>
2009-07-13 12:57:30 +0000

    Fix data-regen rule in documentation.
    	* doc/examples/Makefile.am: fix 'data-regen' rule.
    
    	* doc/tools/split_sample.sh: add support for file name suffix.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4282 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/doc/tools/split_sample.sh
+++ milena/doc/tools/split_sample.sh
@@ -1,23 +1,27 @@
 # /bin/sh
 
-if ! [ $# -eq 3 ]; then
-  echo "Usage: $0 <file1> <file extension> <dst dir>"
+if ! [ $# -eq 4 ]; then
+  echo "Usage: $0 <file1> <file extension> <suffix> <dst dir>"
   exit 1;
 fi
 
 begins=`grep -n -R '//[[:space:]]*\\\{' $1 | cut -d ':' -f 1`
 ends=`grep -n -R '//[[:space:]]*\\\}' $1 | cut -d ':' -f 1`
 
+if ! [ -z "$3" ]; then
+  suffix=".$3"
+fi
+
 split=false
 i=1
 for begin in $begins; do
   partend=$((`echo $ends | cut -d ' ' -f $i` - 1))
   subdir=`basename \`dirname $1\``
-  out_name="`basename $1 .$2`-$i.$2"
+  out_name="`basename $1 .$2`-$i.$2$suffix"
   if [ "$subdir" != "examples" ] && [ "$subdir" != "outputs" ]; then
     out_name="${subdir}_$out_name"
   fi
-  head -n $partend $1 | tail -n $(($partend - $begin)) > "$3/$out_name"
+  head -n $partend $1 | tail -n $(($partend - $begin)) > "$4/$out_name"
   i=$((i+1))
   split=true
 done
