0d241a4526dd3d1d5f20956ebd9066685d7776dc
Guillaume Lazzara <z@lrde.epita.fr>
2009-07-13 12:57:27 +0000

    Update a tutorial example.
    	* doc/outputs/tuto4_site_set_create.txt,
    	* doc/outputs/split/tuto4_site_set_create-1.txt,
    	* doc/outputs/split/tuto4_site_set_create-2.txt,
    	* doc/outputs/split/tuto4_site_set_create-3.txt,
    	* doc/examples/split/tuto4_site_set_create-2.cc.raw,
    	* doc/examples/split/tuto4_site_set_create-3.cc.raw,
    	* doc/examples/split/tuto4_site_set_create-4.cc.raw,
    	* doc/examples/split/tuto4_site_set_create-5.cc.raw: New reference
    	files.
    
    	* doc/examples/tuto4/site_set_create.cc: Add more sample code.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4281 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/doc/examples/tuto4/site_set_create.cc
+++ milena/doc/examples/tuto4/site_set_create.cc
@@ -10,31 +10,43 @@ int main()
   box2d b(4,4);
   // \}
 
-  // \{
-  p_array<point2d> arr;
-  arr.append(point2d(2,2));
-  arr.append(point2d(1,2));
-  // \}
-
-  std::cout << "\\{" << std::endl;
+  std::cout << "// \\{" << std::endl;
   // \{
   mln_piter_(box2d) p(b);
   for_all(p)
     std::cout << p << ", ";
   std::cout << std::endl;
   // \}
-  std::cout << "\\}" << std::endl;
+  std::cout << "// \\}" << std::endl;
+
+
+  {
+    // \{
+    p_array<point2d> arr;
+    arr.append(point2d(2,2));
+    arr.append(point2d(1,2));
+    // \}
+
+    std::cout << "// \\{" << std::endl;
+    // \{
+    mln_piter_(p_array<point2d>) p(arr);
+    for_all(p)
+      std::cout << p << ", ";
+    std::cout << std::endl;
+    // \}
+    std::cout << "// \\}" << std::endl;
+  }
 
   {
     // \{
     box2d b = make::box2d(-2,-2, 2, 2);
     // \}
 
-    std::cout << "\\{" << std::endl;
+    std::cout << "// \\{" << std::endl;
     mln_piter_(box2d) p(b);
     for_all(p)
       std::cout << p << ", ";
     std::cout << std::endl;
-    std::cout << "\\}" << std::endl;
+    std::cout << "// \\}" << std::endl;
   }
 }
