0c4ae17308a637adab6f28791d66e732cfbaa349
Guillaume Lazzara <z@lrde.epita.fr>
2009-08-17 11:46:05 +0000

    Add missing copyright and reference in ICDAR code.
    	* icdar/2009/hsc/input_to_lines_dat.cc,
    	* icdar/2009/hsc/input_to_words_dat.cc: Add copyright and a
    	reference to the paper in which the results are published.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4362 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/icdar/2009/hsc/input_to_lines_dat.cc
+++ milena/sandbox/icdar/2009/hsc/input_to_lines_dat.cc
@@ -1,3 +1,38 @@
+// Copyright (C) 2009 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+
+// Results published in:
+//
+// `ICDAR2009 Handwriting Segmentation Contest`.
+// B. Gatos, N. Stamatopoulos and G. Louloudis
+// 2009 10th International Conference on Document Analysis and Recognition
+//
+// http://www.cvc.uab.es/icdar2009/papers/3725b393.pdf
+
+
 #include <sandbox/icdar/2009/hsc/input_to_lines.hh>
 #include <sandbox/icdar/2009/hsc/io/icdar/save.hh>
 
