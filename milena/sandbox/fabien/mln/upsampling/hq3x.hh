312f6d7e9c0d6014f35850f6ad1353dc02a06f4b
Fabien Freling <freling@lrde.epita.fr>
2009-09-07 10:02:03 +0000

    Add HQ4x algorithm, change license and add credit to original author.
    	*  fabien/mln/upsampling/hq2x.hh: Change license and add credit to
    	original author.
    	*  fabien/mln/upsampling/hq3x.hh: Change license and add credit to
    	original author.
    	*  fabien/mln/upsampling/hq4x.hh: Implement HQ4x algorithm, still
    	not working properly.
    	*  fabien/tests/upsampling/Makefile: Add target `hq4x'.
    	*  fabien/tests/upsampling/hq4x.cc: New test file for hq4x().
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4433 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/sandbox/fabien/mln/upsampling/hq3x.hh
+++ milena/sandbox/fabien/mln/upsampling/hq3x.hh
@@ -1,34 +1,28 @@
-// Copyright (C) 2009 EPITA Research and Development Laboratory (LRDE)
+//Copyright (C) 2003 MaxSt ( maxst@hiend3d.com )
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
 //
-// This file is part of Olena.
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//Lesser General Public License for more details.
 //
-// Olena is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation, version 2 of the License.
-//
-// Olena is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
-//
-// As a special exception, you may use this file as part of a free
-// software project without restriction.  Specifically, if other files
-// instantiate templates or use macros or inline functions from this
-// file, or you compile this file and link it with other files to produce
-// an executable, this file does not by itself cause the resulting
-// executable to be covered by the GNU General Public License.  This
-// exception does not however invalidate any other reasons why the
-// executable file might be covered by the GNU General Public License.
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 
 #ifndef MLN_UPSAMPLING_HQ3X_HH
 # define MLN_UPSAMPLING_HQ3X_HH
 
 /// \file
 ///
-/// Produce an up-scaled image using hq3x algorithm.
+/// Produce an up-scaled image using hq3x algorithm by Maxim Stepin.
+/// Reference can be found at:
+/// http://web.archive.org/web/20070624082212/www.hiend3d.com/hq3x.html
 
 # include <mln/geom/ncols.hh>
 # include <mln/geom/nrows.hh>
