151e7e0395e098e7b79788f0ed89973511c9f7d0
Roland Levillain <roland@lrde.epita.fr>
2009-09-24 15:25:32 +0000

    Fix usage message of apps/constrained-connectivity.
    	* apps/constrained-connectivity/constrained-connectivity.cc (main):
    	s/input.pbm/input.pgm/.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4544 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- milena/apps/constrained-connectivity/constrained-connectivity.cc
+++ milena/apps/constrained-connectivity/constrained-connectivity.cc
@@ -92,7 +92,7 @@ int main(int argc, char* argv[])
 {
   if (argc != 2)
     {
-      std::cerr << "Usage: " << argv[0] << " input.pbm" << std::endl;
+      std::cerr << "Usage: " << argv[0] << " input.pgm" << std::endl;
       std::exit(1);
     }
 
