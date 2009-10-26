7cc47837ab35263b7bf7a3026b976a6911127a3d
Roland Levillain <roland@lrde.epita.fr>
2009-10-26 17:35:19 +0000

    Send dyn patches to the olena-patches mailing list.
    	* vcs/dynamic_static_bridge_vcs.rb: Here.
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/trunk@4674 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- dynamic-use-of-static-c++/vcs/dynamic_static_bridge_vcs.rb
+++ dynamic-use-of-static-c++/vcs/dynamic_static_bridge_vcs.rb
@@ -5,13 +5,9 @@ class Vcs
   protocol_version '0.1'
 
   def dynamic_static_bridge_commit! ( *args )
-
-    common_commit!("[DynamicStaticBridge]: <%= rev %>: <%= title %>", *args) do |subject|
-      
-      mail! :to => ["damien.thivolle@gmail.com", "nicolas.pouillard@gmail.com"], :subject => subject
-      
+    common_commit!("dyn: <%= rev %>: <%= title %>", *args) do |subject|
+      mail!(:to => %w[olena-patches@lrde.epita.fr], :subject => subject)
     end
-
   end
   alias_command  :dystci, :dynamic_static_bridge_commit
   default_commit :dynamic_static_bridge_commit
