0d8376dcf74d9e67e3d8dfe80a31dcefb95f050e
Yann Jacquelet <jacquelet@lrde.epita.fr>
2010-02-10 13:10:01 +0100

    Fix last details in the image processing chain.
    	* green/tools/annotating/histo/histo.cc: Manage new inputs/outputs.
    	* green/tools/annotating/opening/opening.cc: Manage new inputs/outputs.
    	* green/tools/annotating/iz/Makefile.am: New Makefile.
    	* green/tools/annotating/iz/iz.cc: New file.
    	* green/tools/annotating/regmax/regmax.cc: Manage new inputs/outputs.

--- milena/sandbox/green/tools/annotating/iz/iz.cc
+++ milena/sandbox/green/tools/annotating/iz/iz.cc
@@ -1,13 +1,10 @@
-// TOOLS ==> regmax on histo
+// TOOLS ==> influence zone transformation
 
 #include <iostream>
 #include <fstream>
 #include <boost/format.hpp>
 
 #include <mln/accu/stat/histo3d_rgb.hh>
-#include <mln/accu/stat/mean.hh>
-
-#include <mln/algebra/vec.hh>
 
 #include <mln/core/macros.hh>
 #include <mln/core/alias/neighb3d.hh>
@@ -16,29 +13,23 @@
 
 #include <mln/data/compute.hh>
 
-#include <mln/debug/println.hh>
 #include <mln/display/display_histo.hh>
 
 #include <mln/io/dump/load.hh>
 #include <mln/io/dump/save.hh>
-#include <mln/io/pgm/load.hh>
-#include <mln/io/pgm/save.hh>
 #include <mln/io/ppm/load.hh>
 #include <mln/io/ppm/save.hh>
-
-#include <mln/labeling/regional_maxima.hh>
-#include <mln/labeling/compute.hh>
-#include <mln/labeling/mean_values.hh>
+#include <mln/io/pgm/load.hh>
+#include <mln/io/pgm/save.hh>
 
 #include <mln/literal/colors.hh>
 
-#include <mln/morpho/opening/volume.hh>
+#include <mln/labeling/compute.hh>
+#include <mln/labeling/mean_values.hh>
 
-#include <mln/value/label_8.hh>
+#include <mln/transform/influence_zone_geodesic.hh>
+
 #include <mln/value/int_u8.hh>
-#include <mln/value/rgb8.hh>
-
-#include <mln/util/array.hh>
 
 template <unsigned n>
 struct t_labeling_rgbn : mln::Function_v2v< t_labeling_rgbn<n> >
@@ -143,119 +134,175 @@ void compute_stats(const mln::image2d<mln::value::rgb8>&    i_input_rgb8,
   log_stream.close();
 }
 
-void save(mln::util::array< mln::algebra::vec<3,float> >& m2_label,
-	  const char                                      *colormap)
+bool expect(std::istream& stream, const std::string expected)
+{
+  bool        result;
+  std::string got;
+
+  stream >> got;
+
+  result = (got == expected);
+
+  return result;
+}
+
+std::istream& operator>>(std::istream& stream,
+			 mln::algebra::vec<3,float>& color)
+{
+  unsigned lbl;
+
+  stream >> lbl;
+  if (expect(stream, std::string("|")) &&
+      expect(stream, std::string("r")) &&
+      expect(stream, std::string("=")))
+  {
+    stream >> color[0];
+
+    if (expect(stream, std::string(",")) &&
+	expect(stream, std::string("g")) &&
+	expect(stream, std::string("=")))
+    {
+      stream >> color[1];
+
+      if (expect(stream, std::string(",")) &&
+	  expect(stream, std::string("b")) &&
+	  expect(stream, std::string("=")))
+      {
+	stream >> color[2];
+      }
+    }
+  }
+
+  return stream;
+}
+
+void load(mln::util::array< mln::algebra::vec<3,float> >& m2_label,
+	  const char *colormap)
 {
   typedef mln::algebra::vec<3,float> t_vec3f;
   typedef mln::util::array<t_vec3f>  t_mean_array;
 
-  std::ofstream stream(colormap);
+  std::ifstream stream(colormap);
+  std::string   buffer;
 
-  for (unsigned i = 0; i < m2_label.size(); ++i)
+  getline(stream, buffer);
+
+  while (0 < buffer.size())
   {
-    const t_vec3f& mean_v = m2_label[i];
+    std::stringstream line(buffer);
+    t_vec3f           mean_v;
 
-    stream << boost::format("%2i | r = %6.2f, g = %6.2f, b = %6.2f")
-      % i
-      % mean_v[0]
-      % mean_v[1]
-      % mean_v[2]
-	   << std::endl;
+    line >> mean_v;
+
+    m2_label.append(mean_v);
+
+    getline(stream, buffer);
   }
 
-  stream.flush();
   stream.close();
 }
 
-template <unsigned n>
-void mk_regmax(const std::string&   input,   // in
-	       const std::string&   quant,   // in
-	       const std::string&   histo,   // in
-	       const std::string&   opened,  // in
-	       const mln::neighb3d& nbh,     // in
-	       const std::string&   labeled, // out
-	       const std::string&   proj,    // out
-	       const std::string&   colormap,// out
-	       const std::string&   mean,    // out
-	       const std::string&   stats)   // [out]
+template<unsigned n>
+void mk_iz(const std::string&        labeled, // in
+	   const unsigned            d,       // in
+	   const mln::neighb3d&      nbh,     // in
+	   const std::string&        input,   // in
+	   const std::string&        quant,   // in
+	   const std::string&        histo,   // in
+	   const std::string&        colormap,// in
+	   const std::string&        iz,      // out
+	   const std::string&        proj,    // out
+	   const std::string&        mean,    // out
+	   const std::string&        stats)   // [out]
 {
+  typedef mln::value::int_u8                          t_int_u8;
   typedef mln::value::label_8                         t_lbl8;
   typedef mln::value::rgb8                            t_rgb8;
   typedef mln::value::rgb<n>                          t_rgbn;
-  typedef mln::value::int_u8                          t_int_u8;
-  typedef mln::value::int_u<n>                        t_int_un;
   typedef mln::algebra::vec<3,float>                  t_v3f;
   typedef mln::image2d<t_int_u8>                      t_image2d_int_u8;
-  typedef mln::image2d<t_int_un>                      t_image2d_int_un;
   typedef mln::image2d<t_rgb8>                        t_image2d_rgb8;
   typedef mln::image2d<t_rgbn>                        t_image2d_rgbn;
-  typedef mln::image3d<t_lbl8>                        t_image3d_lbl8;
   typedef mln::image2d<t_lbl8>                        t_image2d_lbl8;
   typedef mln::image3d<unsigned>                      t_histo3d;
+  typedef mln::image3d<t_lbl8>                        t_image3d_lbl8;
   typedef mln::accu::meta::stat::histo3d_rgb          t_histo3d_fun;
   typedef mln::accu::stat::mean<t_v3f,t_v3f,t_v3f>    t_mean;
   typedef mln::util::array<t_v3f>                     t_mean_array;
 
+  // START OF IMAGE PROCESSING CHAIN
   t_image2d_rgb8                                      i0_input; // input img
   t_image2d_rgbn                                      i1_input; // quant img
-  t_histo3d                                           h1_input; // input histo
-  t_histo3d                                           h2_input; // opened histo
-  t_image2d_rgb8                                      p2_label; // histo proj
+  t_histo3d                                           h1_input; // histo input
+  t_histo3d                                           h2_input; // histo input
+  t_image2d_int_u8                                    p1_histo; // histo proj
+  t_image2d_rgb8                                      p3_histo; // histo proj
   t_image3d_lbl8                                      l2_histo; // label histo
-  t_image2d_lbl8                                      l2_input; // label input
+  t_image3d_lbl8                                      l3_histo; // iz label hist
   t_mean_array                                        m2_label; // colormap
-  t_image2d_rgb8                                      i2_mean;  // reconstructed
+  t_mean_array                                        m3_label; // colormap
+  t_image2d_lbl8                                      l3_input; // label input
+  t_image2d_rgb8                                      i3_mean;  // reconstructed
 
-  t_lbl8                                              n_lbl;    // nb class
+  t_lbl8                                              n_lbl;    // nb labels
   t_rgb8                                              red(mln::literal::red);
 
-
-  // BEGIN LOADING
+  mln::io::dump::load(l2_histo, labeled.c_str());
   mln::io::ppm::load(i0_input, input.c_str());
   mln::io::ppm::load(i1_input, quant.c_str());
   mln::io::dump::load(h1_input, histo.c_str());
-  mln::io::dump::load(h2_input, opened.c_str());
-  // END LOADING
+  load(m2_label, colormap.c_str());
 
-  // BEGIN IMAGE PROCESSING
-  l2_histo  = mln::labeling::regional_maxima(h2_input, nbh, n_lbl);
-  // END IMAGE PROCESSING
+  if (0 == d)
+  {
+    l3_histo = mln::transform::influence_zone_geodesic(l2_histo, nbh);
+  }
+  else
+  {
+    l3_histo = mln::transform::influence_zone_geodesic(l2_histo, nbh, d);
+  }
+  // END OF IMAGE PROCESSING CHAIN
 
-  // BEGIN SAVING
-  mln::io::dump::save(l2_histo, labeled.c_str());
+  // BEGIN DUMPING
 
-  l2_input = mln::data::transform(i1_input, t_labeling_rgbn<n>(l2_histo));
-  i2_mean  = mln::labeling::mean_values(i0_input, l2_input, n_lbl);
-  m2_label = mln::labeling::compute(t_mean(), i0_input, l2_input, n_lbl);
+  n_lbl    = (t_lbl8)(m2_label.size()-1);
+
+  l3_input = mln::data::transform(i1_input, t_labeling_rgbn<n>(l3_histo));
+  i3_mean  = mln::labeling::mean_values(i0_input, l3_input, n_lbl);
+  m3_label = mln::labeling::compute(t_mean(), i0_input, l3_input, n_lbl);
 
   // CORRECT 0 LABEL STATS
-  m2_label[0][0] = 255.0;
-  m2_label[0][1] = 255.0;
-  m2_label[0][2] = 0.0;
+  m3_label[0][0] = 255.0;
+  m3_label[0][1] = 255.0;
+  m3_label[0][2] = 0.0;
 
-  p2_label =mln::display::display3_histo3d_unsigned<n>(h1_input,
-						       l2_histo,
-						       m2_label,
-						       red);
-
-  mln::io::ppm::save(p2_label, proj.c_str());
-  save(m2_label, colormap.c_str());
-  mln::io::ppm::save(i2_mean, mean.c_str());
+  p3_histo = mln::display::display3_histo3d_unsigned<n>(h1_input,
+							l3_histo,
+							m3_label,
+							red);
+  mln::io::dump::save(l3_input, iz.c_str());
+  mln::io::ppm::save(p3_histo, proj.c_str());
+  mln::io::ppm::save(i3_mean, mean.c_str());
 
   if (0 < stats.size())
-    compute_stats(i0_input, l2_input, h1_input, l2_histo, n_lbl, stats);
+    compute_stats(i0_input, l3_input, h1_input, l3_histo, n_lbl, stats);
 
-  // END SAVING
+  // END DUMPING
 }
+
 
 void usage()
 {
   std::cout                                                       << std::endl;
-  std::cout << "regmax input.ppm q quant.ppm histo.dump"
-	    << " opened.dump nbh labeled.dump proj.ppm"
-	    << " colormap.txt mean.ppm [stats.txt]"               << std::endl;
+  std::cout << "iz labeled.dump d nbh input.ppm q quant.ppm"
+	    << " histo.dump colormap.txt iz.dump proj.ppm"
+	    << " mean.ppm [stats.txt]"                            << std::endl;
   std::cout << std::endl;
   std::cout << "where :"                                          << std::endl;
+  std::cout << "* [ in] labeled.dump is the labeled 3d histogram" << std::endl;
+  std::cout << "* [ in] d is the depth for the zone influence"
+	    << " transformation (0 means infinite)"               << std::endl;
+  std::cout << "* [ in] nbh is the 3d neighbourhood {6,18,26}"    << std::endl;
   std::cout << "* [ in] input.ppm is the 8 bits color ppm image"  << std::endl;
   std::cout << "* [ in] q is the degree of quantification"
 	    << " {2,3,4,5,6,7,8}"                                 << std::endl;
@@ -263,12 +310,10 @@ void usage()
 	    << " image"                                           << std::endl;
   std::cout << "* [ in] histo.dump is the quantified color"
 	    << " histogram"                                       << std::endl;
-  std::cout << "* [ in] opened.dump is the filtered histogram"    << std::endl;
-  std::cout << "* [ in] nbh is the 3d neighbourhood {6,18,26}"    << std::endl;
-  std::cout << "* [out] labeled.dump is the labeled 3d histogram" << std::endl;
+  std::cout << "* [ in] colormap.txt is the colormap for labels"  << std::endl;
+  std::cout << "* [out] iz.dump is the iz labeled 3d histogram"   << std::endl;
   std::cout << "* [out] proj.ppm is the r/g projection of the"
 	    << " histogram with maxima label plot on"             << std::endl;
-  std::cout << "* [out] colormap.txt is the colormap for labels"  << std::endl;
   std::cout << "* [out] mean.ppm is the mean reconstructed image" << std::endl;
   std::cout << "* [out] stats.txt is the statistical label report"<< std::endl;
   std::cout << std::endl;
@@ -276,19 +321,20 @@ void usage()
 
 int main(int argc, char* args[])
 {
-  if (11 == argc || 12 == argc)
+  if (12 == argc || 13 == argc)
   {
-    const std::string input(args[1]);   // in
-    const char        q = args[2][0];   // in
-    const std::string quant(args[3]);   // in
-    const std::string histo(args[4]);   // in
-    const std::string opened(args[5]);  // in
-    const char        nbh = args[6][0]; // in
-    const std::string labeled(args[7]); // out
-    const std::string proj(args[8]);    // out
-    const std::string colormap(args[9]);// out
-    const std::string mean(args[10]);    // out
-    const std::string stats(12 == argc? args[11] : "");   // [out]
+    const std::string labeled(args[1]);  // in
+    const unsigned    d = atoi(args[2]); // in
+    const char        nbh = args[3][0];  // in
+    const std::string input(args[4]);    // in
+    const char        q = args[5][0];    // in
+    const std::string quant(args[6]);    // in
+    const std::string histo(args[7]);    // in
+    const std::string colormap(args[8]); // in
+    const std::string iz(args[9]);       // out
+    const std::string proj(args[10]);     // out
+    const std::string mean(args[11]);    // out
+    const std::string stats(13 == argc? args[12] : "");   // [out]
 
 
     mln::neighb3d neighbourhood;
@@ -303,21 +349,20 @@ int main(int argc, char* args[])
 
     switch (q)
     {
-
-      case '2': mk_regmax<2>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '3': mk_regmax<3>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '4': mk_regmax<4>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '5': mk_regmax<5>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '6': mk_regmax<6>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '7': mk_regmax<7>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
-      case '8': mk_regmax<8>(input,quant,histo,opened,neighbourhood,
-			     labeled,proj,colormap,mean,stats); break;
+      case '2' : mk_iz<2>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '3' : mk_iz<3>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '4' : mk_iz<4>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '5' : mk_iz<5>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '6' : mk_iz<6>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '7' : mk_iz<7>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
+      case '8' : mk_iz<8>(labeled,d,neighbourhood,input,quant,
+			  histo,colormap,iz,proj,mean,stats);break;
       default: usage(); break;
     }
   }
--- milena/sandbox/green/tools/annotating/opening/opening.cc
+++ milena/sandbox/green/tools/annotating/opening/opening.cc
@@ -15,36 +15,47 @@
 
 #include <mln/io/dump/load.hh>
 #include <mln/io/dump/save.hh>
-#include <mln/io/pgm/load.hh>
+#include <mln/io/ppm/save.hh>
 #include <mln/io/pgm/save.hh>
+
+#include <mln/literal/colors.hh>
 
 #include <mln/morpho/opening/volume.hh>
 
+#include <mln/value/rgb.hh>
 #include <mln/value/int_u8.hh>
 
-void mk_opening(const std::string& input,
-		const unsigned     min_vol,
-		const std::string& output,
-		const std::string& opened)
+template <unsigned n>
+void mk_opening(const std::string& histo,   // in
+		const unsigned     min_vol, // in
+		const std::string& opened,  // out
+		const std::string& proj1,   // out
+		const std::string& proj2)   // out
 {
   typedef mln::value::int_u8                          t_int_u8;
+  typedef mln::value::rgb<n>                          t_rgbn;
   typedef mln::image2d<t_int_u8>                      t_image2d_int_u8;
+  typedef mln::image2d<t_rgbn>                        t_image2d_rgbn;
   typedef mln::image3d<unsigned>                      t_histo3d;
   typedef mln::accu::meta::stat::histo3d_rgb          t_histo3d_fun;
 
   // START OF IMAGE PROCESSING CHAIN
   t_histo3d                                           h1_input; // histo input
   t_histo3d                                           h2_input; // histo input
-  t_image2d_int_u8                                    p1_histo; // histo proj
+  t_image2d_int_u8                                    p1_histo1;// histo proj1
+  t_image2d_rgbn                                      p1_histo2;// histo proj2
+  t_rgbn                                              red(mln::literal::red);
 
-  mln::io::dump::load(h1_input, input.c_str());
+  mln::io::dump::load(h1_input, histo.c_str());
   h2_input = mln::morpho::opening::volume(h1_input, mln::c6(), min_vol);
   // END OF IMAGE PROCESSING CHAIN
 
   // BEGIN DUMPING
-  p1_histo = mln::display::display_histo3d_unsigned(h2_input);
+  p1_histo1 = mln::display::display_histo3d_unsigned(h2_input);
+  p1_histo2 = mln::display::display3_histo3d_unsigned(h2_input, red);
   mln::io::dump::save(h2_input, opened.c_str());
-  mln::io::pgm::save(p1_histo, output.c_str());
+  mln::io::pgm::save(p1_histo1, proj1.c_str());
+  mln::io::ppm::save(p1_histo2, proj2.c_str());
   // END DUMPING
 }
 
@@ -52,25 +63,46 @@ void mk_opening(const std::string& input,
 void usage()
 {
   std::cout                                                       << std::endl;
-  std::cout << "opening input.dump v out.dump out.ppm"            << std::endl;
-  std::cout << "where"                                            << std::endl;
-  std::cout << "input.dump is the 3d color input histo"           << std::endl;
-  std::cout << "v is the minimum size of each composant"          << std::endl;
-  std::cout << "out.pgm is the r/g proj of the opened histogram"  << std::endl;
-  std::cout << "out.dump is the opened histogram"                 << std::endl;
+  std::cout << "opening q histo.dump v opened.dump proj.pgm"
+	    << " proj.ppm"                                        << std::endl;
+  std::cout << std::endl;
+  std::cout << "where :"                                          << std::endl;
+  std::cout << "* [ in] q is the degree of quantification"
+	    << " {2,3,4,5,6,7,8}"                                 << std::endl;
+  std::cout << "* [ in] histo.dump is the quantified color"
+	    << " histogram"                                       << std::endl;
+  std::cout << "* [ in] v is the minimum size (in pixels) of"
+	    << " each composant"                                  << std::endl;
+  std::cout << "* [out] opened.dump is the filtered histogram"    << std::endl;
+  std::cout << "* [out] proj.pgm is the r/g projection of the"
+	    << " histogram (summing along the blue axe)"          << std::endl;
+  std::cout << "* [out] proj.ppm is the r/g projection of the"
+	    << " histogram with maxima plot on"                   << std::endl;
   std::cout << std::endl;
 }
 
 int main(int argc, char* args[])
 {
-  if (5 == argc)
+  if (7 == argc)
   {
-    const std::string input(args[1]);
-    const unsigned    min_vol = atoi(args[2]);
-    const std::string output(args[3]);
-    const std::string opened(args[4]);
+    const char        q       = args[1][0];    // in
+    const std::string histo(args[2]);          // in
+    const unsigned    min_vol = atoi(args[3]); // in
+    const std::string opened(args[4]);         // out
+    const std::string proj1(args[5]);          // out
+    const std::string proj2(args[6]);          // out
 
-    mk_opening(input, min_vol, output, opened);
+    switch(q)
+    {
+      case '2': mk_opening<2>(histo, min_vol, opened, proj1, proj2); break;
+      case '3': mk_opening<3>(histo, min_vol, opened, proj1, proj2); break;
+      case '4': mk_opening<4>(histo, min_vol, opened, proj1, proj2); break;
+      case '5': mk_opening<5>(histo, min_vol, opened, proj1, proj2); break;
+      case '6': mk_opening<6>(histo, min_vol, opened, proj1, proj2); break;
+      case '7': mk_opening<7>(histo, min_vol, opened, proj1, proj2); break;
+      case '8': mk_opening<8>(histo, min_vol, opened, proj1, proj2); break;
+      default:  usage(); break;
+    }
   }
   else
     usage();
