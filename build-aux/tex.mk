d9fedc37413c7aeed75e85fb7c12e906f980aa5a
Roland Levillain <roland@lrde.epita.fr>
2010-03-12 16:20:49 +0100

    Add texi2dvi as well as a Make helper to use it.
    	* build-aux/texi2dvi: New script.
    	Imported from Texinfo.
    	* build-aux/tex.mk: New Makefile helper.
    	Inspired by LRDE share/style/tex.mk.
    	* build-aux/Makefile.am (EXTRA_DIST): Add texi2dvi.

---
+++ build-aux/tex.mk
@@ -0,0 +1,40 @@
+## tex.mk 					-*- Makefile-Automake -*-
+
+## A small subset of LRDE's share/make/tex.mk.
+
+## Copyright (C) 2010 EPITA Research and Development Laboratory (LRDE).
+##
+## This file is part of Olena.
+##
+## Olena is free software: you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free
+## Software Foundation, version 2 of the License.
+##
+## Olena is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+
+build_aux_dir = $(top_srcdir)/build-aux
+
+TEXI2DVI = $(build_aux_dir)/texi2dvi
+TEXI2DVI_FLAGS = --tidy --build-dir=tmp.t2d --batch
+
+
+TEXI2PDF = $(TEXI2DVI) --pdf
+TEXI2PDF_FLAGS = $(TEXI2DVI_FLAGS)
+
+SUFFIXES = .pdf .tex
+
+.tex.pdf:
+	$(TEXI2PDF) $(TEXI2PDF_FLAGS) -o $@ $<
+
+tex-mostlyclean:
+	rm -rf tmp.t2d
+.PHONY: tex-mostlyclean
+# mostlyclean-local is an Automake special target.
+mostlyclean-local: tex-mostlyclean
+.PHONY: mostlyclean-local
