0f9327c948c0cf9933a967499150f492fe63094f
Roland Levillain <roland@lrde.epita.fr>
2010-03-12 18:26:00 +0100

    Split LaTeX-to-HTML rules to clear things up.
    	* doc/tools/todoxygen.sh: Take a HTML file as input instead of a
    	LaTeX one.
    	* doc/Makefile.am (DOC_PACKAGES): New variable.
    	(TECHNICAL_HTML, TUTORIAL_HTML, REF_GUIDE_HTML):
    	New variables.
    	($(TECHNICAL_HTML), $(TUTORIAL_HTML), $(REF_GUIDE_HTML)):
    	New targets.
    	(dist_doc_DATA): Add $(TECHNICAL_HTML), $(TUTORIAL_HTML) and
    	$(REF_GUIDE_HTML).
    	($(TECHNICAL_HH)): Depend on and process $(TECHNICAL_HTML) instead
    	of $(technical_dir)/technical.tex.
    	($(TUTORIAL_HH)): Depend on and process $(TUTORIAL_HTML) instead
    	of $(tutorial_dir)/tutorial.tex.
    	($(REF_GUIDE_HH)): Depend on and process $(REF_GUIDE_HTML) instead
    	of $(ref_guide_dir)/ref_guide.tex.
    	(CLEANFILES): Remove now useless patterns.
    	(clean-local): Likewise.
    	Adjust paths.

--- milena/doc/tools/todoxygen.sh
+++ milena/doc/tools/todoxygen.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#! /bin/sh
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <file.tex> <output dir> <path to .sty>";
-  exit 1;
-fi
+# todoxygen.sh: Turn HTML files into suitable inputs for doxygen.
 
-out="$2/`basename $1 .tex`.hh"
-html="$2/`basename $1 .tex`.html"
+# Copyright (C) 2009, 2010 EPITA Research and Development Laboratory (LRDE).
+#
+# This file is part of Olena.
+#
+# Olena is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2 of the License.
+#
+# Olena is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Olena.  If not, see <http://www.gnu.org/licenses/>.
 
-DOC_PACKAGES="
-  $3/doxyfuns.sty \
-  $3/milena.sty"
+test $# -eq 3 \
+  || { echo "Usage: $0 <file.html> <output dir> <path to .sty>"; exit 1; }
 
-#Convert .tex to .html
-hevea -O -fix $DOC_PACKAGES $1 -o $html
+html="$1"
+out="$2/`basename $1 .html`.hh"
 
 bodyl=`grep -n "<BODY >" $html | cut -d ':' -f 1`
 nlines=`wc -l $html | cut -d ' ' -f 1`
@@ -32,11 +41,10 @@ sed -i -e 's/&#X201C/\&ldquo/g' $out
 sed -i -e 's/&#X201D/\&rdquo/g' $out
 sed -i -e 's/&#X2261/\&equiv/g' $out
 
-#Doxygen wants us to preserve '\n' after commands. 
-#\ref and \see do not need that extra new line.
+# Doxygen wants us to preserve '\n' after commands. 
+# \ref and \see do not need that extra new line.
 for keyword in include section page subpage subsection subsubsection image; do
   sed -i -e "s/\\\\endhtmlonly\\\\$keyword/\\\\endhtmlonly\n\n\\\\$keyword/g" $out
   sed -i -e "s/\\\\endhtmlonly\*\//\\\\endhtmlonly\n\n\*\/\n\n/g" $out
   sed -i -e "s/\\\\$keyword \(.*\)\\\\htmlonly/\\\\$keyword \1\n\n\\\\htmlonly\n/g" $out
 done
-
