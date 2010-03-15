77004f3748794e5990deca22ecbafb173fd44eef
Roland Levillain <roland@lrde.epita.fr>
2010-03-15 11:07:06 +0100

    Aesthetic changes.
    	* tests/tests.mk,
    	* tests/tests-recursive.mk:
    	Here.
    	Help Emacs find out the nature of these files.

--- milena/tests/tests-recursive.mk
+++ milena/tests/tests-recursive.mk
@@ -1,23 +1,23 @@
-# tests-recursive.mk					-*- Automake -*-
+## tests-recursive.mk				-*- Makefile-Automake -*-
 
-# Copyright (C) 2008, 2009 EPITA Research and Development Laboratory
-# (LRDE)
-#
-# This file is part of Olena.
-#
-# Olena is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation, version 2 of the License.
-#
-# Olena is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+## Compile tests recursively, but don't run them.
 
-# Compile tests recursively, but don't run them.
+## Copyright (C) 2008, 2009 EPITA Research and Development Laboratory
+## (LRDE)
+##
+## This file is part of Olena.
+##
+## Olena is free software: you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free
+## Software Foundation, version 2 of the License.
+##
+## Olena is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Olena.  If not, see <http://www.gnu.org/licenses/>.
 
 include $(top_srcdir)/build-aux/extra-recursive-targets.mk
 
