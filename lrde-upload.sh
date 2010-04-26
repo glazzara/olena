7b351ecac19fd1c42ae0382ef7179496a1e565c8
Roland Levillain <roland@lrde.epita.fr>
2010-04-26 18:19:43 +0200

    Update lrde-upload.sh.
    	* lrde-upload.sh: Catch up with the tree's current layout.

--- lrde-upload.sh
+++ lrde-upload.sh
@@ -3,9 +3,7 @@
 # This file is run by the LRDE autobuilder after a successful compilation.
 # It is not meant to be distributed with Olena.
 
-set -e
-set -x
-
+set -ex
 
 DEST=/lrde/dload/olena/snapshots
 DEST_DOC=$DEST/doc/milena
@@ -18,19 +16,18 @@ REV=$VERSION-snapshot-$CURRENT_DATE
 # Always do "cp then mv" when uploading the file, so that nobody
 # can start a download while the destination file is incomplete.
 
-# Upload the tarball.
 mkdir -p $DEST
 
 # Delete tarballs older than 2 days.
 find $DEST -maxdepth 1 -type f -mtime +1 -name 'olena-*-snapshot-*' \
   -exec rm -f {} \;
 
-# tar.gz
+# Upload the `.tar.gz' tarball.
 cp -f olena-$VERSION.tar.gz $DEST/olena-$REV.tar.gz.tmp
 mv -f $DEST/olena-$REV.tar.gz.tmp $DEST/olena-$REV.tar.gz
 chmod -R a+r $DEST/olena-$REV.tar.gz
 
-# tar.bz2
+# Upload the `.tar.bz2' tarball.
 cp -f olena-$VERSION.tar.bz2 $DEST/olena-$REV.tar.bz2.tmp
 mv -f $DEST/olena-$REV.tar.bz2.tmp $DEST/olena-$REV.tar.bz2
 chmod -R a+r $DEST/olena-$REV.tar.bz2
@@ -39,32 +36,31 @@ chmod -R a+r $DEST/olena-$REV.tar.bz2
 # Upload a copy of the reference manual and other documentation.
 mkdir -p $DEST_DOC
 
-rm -rf $DEST_DOC/user_doc.tmp
-rm -rf $DEST_DOC/white_paper_html.tmp
+cp milena/doc/ref-guide.pdf   $DEST_DOC
+cp milena/doc/tutorial.pdf    $DEST_DOC
+cp milena/doc/user-refman.pdf $DEST_DOC
+cp milena/doc/white-paper.pdf $DEST_DOC
 
+rm -rf $DEST_DOC/user-refman.tmp
+rm -rf $DEST_DOC/white-paper.tmp
 
-# `make distcheck' sets umask to 077.  Restoring permissions for all
-chmod -R a+rx milena/doc/user-refman-html
-chmod -R a+rx milena/doc/white_paper
+# `make distcheck' sets umask to 077.  Restore read permissions for all.
+chmod -R a+rX milena/doc/user-refman
+chmod -R a+rX milena/doc/white-paper
 
-cp -pR milena/doc/user-refman-html $DEST_DOC/user_doc.tmp
-cp -pR milena/doc/white_paper/white_paper_html $DEST_DOC/white_paper_html.tmp
+cp -pR milena/doc/user-refman $DEST_DOC/user-refman.tmp
+cp -pR milena/doc/white-paper $DEST_DOC/white-paper.tmp
 
-cp milena/doc/ref_guide/ref_guide.pdf $DEST_DOC/
-cp milena/doc/tutorial/tutorial.pdf $DEST_DOC/
-cp milena/doc/user-refman.pdf $DEST_DOC/user_doc.pdf
-cp milena/doc/white_paper/white_paper.pdf $DEST_DOC/
+mv -f $DEST_DOC/user-refman $DEST_DOC/user-refman.old
+mv -f $DEST_DOC/white-paper $DEST_DOC/white-paper.old
 
-mv -f $DEST_DOC/user_doc $DEST_DOC/user_doc.old
-mv -f $DEST_DOC/white_paper_html $DEST_DOC/white_paper_html.old
+mv -f $DEST_DOC/user-refman.tmp $DEST_DOC/user-refman
+mv -f $DEST_DOC/white-paper.tmp $DEST_DOC/white-paper
 
-mv -f $DEST_DOC/user_doc.tmp $DEST_DOC/user_doc
-mv -f $DEST_DOC/white_paper_html.tmp $DEST_DOC/white_paper_html
-
-rm -rf $DEST_DOC/user_doc.old
-rm -rf $DEST_DOC/white_paper_html.old
+rm -rf $DEST_DOC/user-refman.old
+rm -rf $DEST_DOC/white-paper.old
 
 # We want to be able to modify these files with both the `build' and
 # `doc' accounts.
-chmod -R g+w $DEST_DOC/user_doc
-chmod -R g+w $DEST_DOC/white_paper_html
+chmod -R g+w $DEST_DOC/user-refman
+chmod -R g+w $DEST_DOC/white-paper
