b48a2f4d78c32cdf341a0e7c53d08540c7744b95
Yann Jacquelet <jacquelet@lrde.epita.fr>
2010-06-21 10:03:10 +0200

    Add milena library components.
    	* green/mln/accu/stat/histo1d.hh: New header component.
    	* green/mln/fun/p2b/achromatic.hh: New header component.
    	* green/mln/fun/p2b/component_equals.hh: New header component.
    	* green/mln/fun/v2v/achromatism.hh: New header component.
    	* green/mln/fun/v2v/hue_concentration.hh: New header component.
    	* green/mln/fun/v2v/rgb_to_achromatism_map.hh: New header component.
    	* green/mln/fun/v2v/rgb_to_hsv.hh: New header component.
    	* green/mln/fun/v2v/rgb_to_hue_map.hh: New header component.
    	* green/mln/fun/v2v/rgb_to_saturation_map.hh: New header component.
    	* green/mln/fun/v2v/rgb_to_value_map.hh: New header component.
    	* green/mln/img_path.hh: New header component.
    	* green/mln/math/ceil.hh: New header component.
    	* green/mln/math/floor.hh: New header component.
    	* green/mln/math/hsv.hh: New header component.

--- milena/sandbox/green/mln/accu/stat/histo1d.hh
+++ milena/sandbox/green/mln/accu/stat/histo1d.hh
@@ -76,26 +76,26 @@
 
 namespace mln
 {
-  
+
   namespace accu
   {
-    
+
     namespace stat
     {
-   
+
       // Forward declaration
       template <typename V>
       struct histo1d;
 
     } // end of namespace mln::accu::stat
-  
+
 
     namespace meta
     {
-    
+
       namespace stat
       {
-	
+
 	struct histo1d : public Meta_Accumulator<histo1d>
 	{
 	  template <typename V>
@@ -104,16 +104,16 @@ namespace mln
 	    typedef accu::stat::histo1d<V> ret;
 	  };
 	};
-	
+
       } // end of namespace mln::accu::meta::stat
-      
+
     } // end of namespace mln::accu::meta
-    
+
   } // end of namespace mln::accu
 
   namespace trait
   {
-    
+
     template <typename V>
     struct accumulator_< mln::accu::stat::histo1d<V> >
     {
@@ -124,34 +124,34 @@ namespace mln
     };
 
     template <typename V>
-    struct set_precise_binary_<op::eq, 
+    struct set_precise_binary_<op::eq,
 			       accu::stat::histo1d<V>,
 			       accu::stat::histo1d<V> >
     {
       typedef bool ret;
     };
-        
+
   } // end of namespace mln::trait
 
   namespace accu
   {
-    
-    namespace stat
-    {      
 
-      /// \brief Define an histogram which returns an image1d .  
+    namespace stat
+    {
+
+      /// \brief Define an histogram which returns an image1d .
       ///
       /// Param V defines the space in which we count the values.
       /// For instance, this histogram works with image2d<int_u8> or with
       /// image1d<int_u<14> >. The histogram count the occurrence of each value.
       /// The number of bins depends of the graylevel values, for 8 bits there
-      /// is 256 bins, for 14 bits there is 16384 bins. Note that over 
+      /// is 256 bins, for 14 bits there is 16384 bins. Note that over
       /// quantification works too (up to 14 bits).
       ///
       /// \ingroup modaccuvalues
 
       template <typename V>
-      struct histo1d : 
+      struct histo1d :
 	public mln::accu::internal::base<image1d<unsigned>, histo1d<V> >
       {
 	typedef V                 argument;
@@ -162,22 +162,22 @@ namespace mln
 	/// \{
 	/// \brief Initialize the size of the resulting image1d.
 	///
-	/// Initialize the size the resulting image from the theorical dynamic 
+	/// Initialize the size the resulting image from the theorical dynamic
 	/// of the graylevel values (Use V to manage it).
 	histo1d();
 	/// \}
 
-	
+
 	/// Manipulators.
 	/// \{
 	/// \brief Initialize the histogram with zero value.
 	///
-	/// This method must be called just before starting the use of the 
-	/// histogram. If it's not, resulting values won't converge to the 
+	/// This method must be called just before starting the use of the
+	/// histogram. If it's not, resulting values won't converge to the
 	/// density.
 	void init();
 
-	
+
 	/// \brief Update the histogram with the graylevel of the pixel t.
 	/// \param[in] t a graylevel pixel of type V.
 	///
@@ -189,7 +189,7 @@ namespace mln
 	/// \brief Update the histogram with an other histogram.
 	/// \param[in] other the other histogram.
 	///
-	/// The end user shouldn't call this method. This is part of 
+	/// The end user shouldn't call this method. This is part of
 	/// data compute interface mechanism.
 
 	void take(const histo1d<V>& other);
@@ -234,7 +234,7 @@ namespace mln
 	typedef mln_trait_value_comp(V,0) comp;
 	typedef point<grid::tick, V>      v_point1d;
 	typedef box<v_point1d>            v_box1d;
-	
+
 	// comp keep a trace of the dimension of the theorical image.
 	// mln_min(comp) --> 0
 	// mln_max(comp) --> 2^(n-1) [127 for n=7][255 for n=8] ...
@@ -242,8 +242,8 @@ namespace mln
 	count_.init_(box1d(point1d(mln_min(comp)),
 			   point1d(mln_max(comp))));
 
-	// std::cout << "min : " << mln_min(comp) << std::endl;
-	// std::cout << "max : " << mln_max(comp) << std::endl;
+// 	std::cout << "min : " << mln_min(comp) << std::endl;
+// 	std::cout << "max : " << mln_max(comp) << std::endl;
 
 	trace::exiting("mln::accu::stat::histo1d<V>::histo1d");
       }
@@ -264,14 +264,14 @@ namespace mln
       {
 	trace::entering("mln::accu::stat::histo1d<V>::take");
 
-	// Just convert a graylevel value (int_u8 like) to a position for an 
+	// Just convert a graylevel value (int_u8 like) to a position for an
 	// iterator on the resulting image.
 	++count_(point1d(t));
 
 	trace::exiting("mln::accu::stat::histo1d<V>::take");
       }
 
-      
+
       template <typename V>
       inline
       void histo1d<V>::take(const histo1d<V>& other)
@@ -282,7 +282,7 @@ namespace mln
 
 	trace::exiting("mln::accu::stat::histo1d<V>::take");
       }
-      
+
       template <typename V>
       inline
       typename histo1d<V>::result histo1d<V>::to_result() const
@@ -292,7 +292,7 @@ namespace mln
 	trace::exiting("mln::accu::stat::histo1d<V>::to_result");
 	return count_;
       }
-      
+
       template <typename V>
       inline
       histo1d<V>::operator result() const
@@ -301,7 +301,7 @@ namespace mln
 
 	trace::exiting("mln::accu::stat::histo1d<V>::operator result");
 	return count_;
-      }      
+      }
 
       template <typename V>
       inline
@@ -309,7 +309,7 @@ namespace mln
       {
 	trace::entering("mln::accu::stat::histo1d<V>::is_valid");
 	bool result = count_.is_valid();
-	
+
 	trace::exiting("mln::accu::stat::histo1d<V>::is_valid");
 	return result;
       }
@@ -322,7 +322,7 @@ namespace mln
 	bool  result                  = true;
 	const image1d<unsigned>& res1 = histo1.to_result();
 	const image1d<unsigned>& res2 = histo2.to_result();
-	
+
 	mln_precondition(res1.is_valid());
 	mln_precondition(res2.is_valid());
 
