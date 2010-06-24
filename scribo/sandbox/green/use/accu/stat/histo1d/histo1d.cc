b0279f5f9bb303de29b8b25783b3a7860361678c
Yann Jacquelet <jacquelet@lrde.epita.fr>
2010-06-24 11:34:46 +0200

    Import files from milena/sandbox/green.
    	* mln/accu/stat/histo1d.hh: New header file.
    	* mln/accu/stat/histo2d.hh: New header file.
    	* mln/accu/stat/histo3d_hsl.hh: New header file.
    	* mln/accu/stat/histo3d_rgb.hh: New header file.
    	* mln/clustering/k_mean.hh: New header file.
    	* mln/clustering/kmean1d.hh: New header file.
    	* mln/clustering/kmean2d.hh: New header file.
    	* mln/clustering/kmean3d.hh: New header file.
    	* mln/clustering/kmean_rgb.hh: New header file.
    	* mln/display/display_histo.hh: New header file.
    	* mln/display/project_histo.hh: New header file.
    	* mln/fun/p2b/achromatic.hh: New header file.
    	* mln/fun/p2b/component_equals.hh: New header file.
    	* mln/fun/v2v/achromatism.hh: New header file.
    	* mln/fun/v2v/hue_concentration.hh: New header file.
    	* mln/fun/v2v/int_u16_to_int_u14.hh: New header file.
    	* mln/fun/v2v/int_u16_to_int_u14.hh: New header file.
    	* mln/fun/v2v/log.hh: New header file.
    	* mln/fun/v2v/rg_to_rgb.hh: New header file.
    	* mln/fun/v2v/rgb8_to_int_u8: New header file.
    	* mln/fun/v2v/rgb_to_achromastism_map.hh: New header file.
    	* mln/fun/v2v/rgb_to_hsv.hh: New header file.
    	* mln/fun/v2v/rgb_to_hue_map.hh: New header file.
    	* mln/fun/v2v/rgb_to_saturation_map.hh: New header file.
    	* mln/fun/v2v/rgb_to_value_map.hh: New header file.
    	* mln/img_path.hh: New header file.
    	* mln/io/plot/save_image_sh.hh: New header file.
    	* mln/math/cell.hh: New header file.
    	* mln/math/floor.hh: New header file.
    	* tests/accu/stat/histo1d/Makefile.am: New makefile.
    	* tests/accu/stat/histo1d/histo1d.cc: New source.
    	* tests/accu/stat/histo2d/Makefile.am: New makefile.
    	* tests/accu/stat/histo2d/histo2d.cc: New source.

---
+++ scribo/sandbox/green/use/accu/stat/histo1d/histo1d.cc
@@ -0,0 +1,24 @@
+/// \file
+///
+/// \brief Minimal code for building 1d image histogram version.
+///
+
+#include <mln/accu/stat/histo1d.hh>
+#include <mln/data/compute.hh>
+#include <mln/core/image/image1d.hh>
+#include <mln/core/image/image2d.hh>
+#include <mln/img_path.hh>
+#include <mln/io/pgm/load.hh>
+#include <mln/value/int_u8.hh>
+
+int main()
+{
+  typedef mln::value::int_u8 t_int_u8;
+  mln::image2d<t_int_u8>     img;
+  mln::image1d<unsigned>     histo;
+
+  mln::io::pgm::load(img, OLENA_IMG_PATH"/lena.pgm");
+  histo = mln::data::compute(mln::accu::meta::stat::histo1d(), img);
+
+  return 0;
+}
