8559cd77490d273ee43ab417b4b666aeb12a3f4b
Guillaume Lazzara <z@lrde.epita.fr>
2010-08-09 11:03:28 +0200

    Add missing includes and headers in AFP's dedicated code.
    	* src/afp/components.hh,
    	* src/afp/link.hh,
    	* src/afp/regroup.hh: Add missing includes and headers.

--- scribo/src/afp/components.hh
+++ scribo/src/afp/components.hh
@@ -1,21 +1,53 @@
-#include <mln/io/pbm/load.hh>
-#include <mln/io/pgm/save.hh>
+// Copyright (C) 2010 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
 
-#include <mln/extension/adjust.hh>
-#include <mln/extension/fill.hh>
-#include <mln/data/fill.hh>
-#include <mln/accu/shape/bbox.hh>
+#ifndef SCRIBO_SRC_AFP_COMPONENTS_HH
+# define SCRIBO_SRC_AFP_COMPONENTS_HH
 
-#include <mln/core/alias/neighb2d.hh>
-#include <mln/core/image/dmorph/image_if.hh>
-#include <mln/pw/value.hh>
-#include <mln/debug/println.hh>
+/// \file
+///
+/// Specific implementation for AFP use case.
 
-#include <mln/util/timer.hh>
-#include <mln/labeling/foreground.hh>
-#include <mln/labeling/wrap.hh>
-#include <mln/extension/fill.hh>
-#include <mln/data/compare.hh>
+# include <mln/io/pbm/load.hh>
+# include <mln/io/pgm/save.hh>
+
+# include <mln/extension/adjust.hh>
+# include <mln/extension/fill.hh>
+# include <mln/data/fill.hh>
+# include <mln/accu/shape/bbox.hh>
+
+# include <mln/core/alias/neighb2d.hh>
+# include <mln/core/image/dmorph/image_if.hh>
+# include <mln/pw/value.hh>
+# include <mln/debug/println.hh>
+
+# include <mln/util/timer.hh>
+# include <mln/labeling/foreground.hh>
+# include <mln/labeling/wrap.hh>
+# include <mln/extension/fill.hh>
+# include <mln/data/compare.hh>
 
 
 namespace mln
@@ -193,3 +225,4 @@ namespace mln
 } // mln
 
 
+#endif // ! SCRIBO_SRC_AFP_COMPONENTS_HH
--- scribo/src/afp/link.hh
+++ scribo/src/afp/link.hh
@@ -1,8 +1,43 @@
-#include <mln/geom/ncols.hh>
-#include <mln/geom/nrows.hh>
-#include <mln/util/couple.hh>
-#include <scribo/core/component_set.hh>
-#include <scribo/core/macros.hh>
+// Copyright (C) 2010 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+#ifndef SCRIBO_SRC_AFP_LINK_HH
+# define SCRIBO_SRC_AFP_LINK_HH
+
+/// \file
+///
+/// Specific implementation for AFP use case.
+
+# include <mln/geom/ncols.hh>
+# include <mln/geom/nrows.hh>
+# include <mln/util/couple.hh>
+# include <mln/math/max.hh>
+
+# include <scribo/core/object_links.hh>
+# include <scribo/core/component_set.hh>
+# include <scribo/core/macros.hh>
 
 namespace scribo
 {
@@ -12,6 +47,8 @@ namespace scribo
 
     namespace link
     {
+
+      using namespace mln;
 
 
       template <typename L>
@@ -137,3 +174,5 @@ namespace scribo
   } // end of namespace scribo::primitive
 
 } // end of namespace scribo
+
+#endif // ! SCRIBO_SRC_AFP_LINK_HH
