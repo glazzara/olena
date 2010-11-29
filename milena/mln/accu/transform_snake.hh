6331ba74dc0221afdae5e0cc95ae8a9dc5ad5439
Roland Levillain <roland@lrde.epita.fr>
2010-11-29 17:58:59 +0100

    Clean up white space.
    	* mln/core/site_set/p_graph_piter.hh,
    	* mln/accu/transform_snake.hh:
    	Here.

--- milena/mln/accu/transform_snake.hh
+++ milena/mln/accu/transform_snake.hh
@@ -451,7 +451,7 @@ namespace mln
 
     template <typename A, typename I, typename W>
     inline
-		 mln_ch_value(I, mln_meta_accu_result(A, mln_value(I)))
+    mln_ch_value(I, mln_meta_accu_result(A, mln_value(I)))
     transform_snake(const Meta_Accumulator<A>& a,
 		    const Image<I>& input, const Window<W>& win)
     {
