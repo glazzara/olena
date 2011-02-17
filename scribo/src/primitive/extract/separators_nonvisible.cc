23fe97d907eae9b7cc76e8f596246615362d98af
Guillaume Lazzara <z@lrde.epita.fr>
2011-02-17 14:23:40 +0100

    Add new tools in Scribo.
    	* src/primitive/extract/Makefile.am,
    	* src/primitive/remove/Makefile.am: Add new targets.
    
    	* src/primitive/extract/separators_nonvisible.cc,
    	* src/primitive/remove/separators.cc: New.

---
+++ scribo/src/primitive/extract/separators_nonvisible.cc
@@ -0,0 +1,60 @@
+// Copyright (C) 2011 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+#include <mln/core/image/image2d.hh>
+#include <mln/io/pbm/all.hh>
+#include <mln/data/convert.hh>
+
+#include <scribo/primitive/extract/separators_nonvisible2.hh>
+#include <scribo/debug/usage.hh>
+
+const char *args_desc[][2] =
+{
+  { "input.pbm", "A binary image." },
+  { "output.pbm", "Output image." },
+  {0, 0}
+};
+
+
+int main(int argc, char *argv[])
+{
+  using namespace mln;
+  using namespace scribo;
+
+  if (argc != 3)
+    return scribo::debug::usage(argv,
+				"Extract non visible separators (whitespaces)",
+				"input.pbm output.pbm",
+				args_desc);
+
+  trace::entering("main");
+
+  image2d<bool> input;
+  io::pbm::load(input, argv[1]);
+
+  io::pbm::save(primitive::extract::separators_nonvisible(input), argv[2]);
+
+  trace::exiting("main");
+}
