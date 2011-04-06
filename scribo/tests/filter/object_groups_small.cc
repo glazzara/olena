52a1107eac0fb8d75c28dc12cb5ed9ee798d2ad0
Guillaume Lazzara <z@lrde.epita.fr>
2011-04-06 15:18:51 +0200

    Add more tests.
    	* tests/filter/Makefile.am: Add new targets.
    
    	* tests/filter/object_groups_mean_width.cc,
    	* tests/filter/object_groups_size_ratio.cc,
    	* tests/filter/object_groups_small.cc,
    	* tests/filter/object_groups_with_holes.cc,
    	* tests/filter/object_links_bbox_h_ratio.cc,
    	* tests/filter/object_links_bbox_overlap.cc,
    	* tests/filter/object_links_bbox_w_ratio.cc,
    	* tests/filter/object_links_bottom_aligned.cc,
    	* tests/filter/object_links_center_aligned.cc,
    	* tests/filter/object_links_left_aligned.cc,
    	* tests/filter/object_links_right_aligned.cc,
    	* tests/filter/object_links_top_aligned.cc: New.
    
    	* scribo/tests/img/the_valleys.pbm: New test data.

---
+++ scribo/tests/filter/object_groups_small.cc
@@ -0,0 +1,87 @@
+// Copyright (C) 2011 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+/// \file
+
+#include <iostream>
+
+#include <mln/core/image/image2d.hh>
+#include <mln/core/alias/neighb2d.hh>
+#include <mln/io/pbm/load.hh>
+#include <scribo/core/def/lbl_type.hh>
+#include <scribo/primitive/extract/components.hh>
+#include <scribo/primitive/link/with_single_left_link.hh>
+#include <scribo/primitive/group/from_single_link.hh>
+#include <scribo/filter/object_groups_small.hh>
+
+#include "tests/data.hh"
+
+int main()
+{
+  using namespace scribo;
+  using namespace mln;
+
+  std::string img = SCRIBO_IMG_DIR "/the_valleys.pbm";
+
+  const unsigned ref[] = { 0, 1, 7, 3, 4, 3, 3, 7, 7, 3, 3, 3, 3 };
+  const unsigned filtered_ref[] = { 0, 0, 0, 3, 0, 3, 3, 0, 0, 3, 3, 3, 3 };
+  const unsigned size_ref[] = { 0, 1, 0, 7, 1, 0, 0, 3, 0, 0, 0, 0, 0 };
+
+
+  image2d<bool> input;
+  io::pbm::load(input, img.c_str());
+
+  typedef scribo::def::lbl_type V;
+  typedef image2d<V> L;
+
+  V nbboxes;
+  component_set<L>
+    text = primitive::extract::components(input, c8(), nbboxes);
+  object_links<L> links = primitive::link::with_single_left_link(text, 30);
+
+  object_groups<L> groups = primitive::group::from_single_link(links);
+
+  for_all_groups(g, groups)
+    mln_assertion(groups(g) == ref[g]);
+
+  {
+    object_groups<L> groups2 = filter::object_groups_small(groups, 4);
+
+    for_all_groups(g, groups2)
+      mln_assertion(groups2(g) == filtered_ref[g]);
+  }
+
+  {
+    mln::util::array<unsigned> group_size;
+    object_groups<L> groups2 = filter::object_groups_small(groups, 4, group_size);
+
+    for_all_groups(g, groups2)
+    {
+      mln_assertion(groups2(g) == filtered_ref[g]);
+      mln_assertion(group_size(g) == size_ref[g]);
+    }
+  }
+
+}
---
+++ scribo/tests/filter/object_links_right_aligned.cc
@@ -0,0 +1,69 @@
+// Copyright (C) 2011 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+/// \file
+
+#include <iostream>
+
+#include <mln/core/image/image2d.hh>
+#include <mln/core/alias/neighb2d.hh>
+#include <mln/io/pbm/load.hh>
+#include <scribo/core/def/lbl_type.hh>
+#include <scribo/primitive/extract/components.hh>
+#include <scribo/primitive/link/with_single_left_link.hh>
+#include <scribo/filter/object_links_right_aligned.hh>
+
+#include "tests/data.hh"
+
+int main()
+{
+  using namespace scribo;
+  using namespace mln;
+
+  std::string img = SCRIBO_IMG_DIR "/phillip.pbm";
+
+  const unsigned ref[] = { 0, 2, 6, 8, 3, 4, 6, 9, 2, 4 };
+  const unsigned filtered_ref[] = { 0, 2, 2, 8, 4, 4, 6, 9, 2, 4 };
+
+  image2d<bool> input;
+  io::pbm::load(input, img.c_str());
+
+  typedef scribo::def::lbl_type V;
+  typedef image2d<V> L;
+
+  V nbboxes;
+  component_set<L>
+    text = primitive::extract::components(input, c8(), nbboxes);
+
+  object_links<L> links = primitive::link::with_single_left_link(text, 30);
+
+  for_all_links(l, links)
+    mln_assertion(links(l) == ref[l]);
+
+  links = scribo::filter::object_links_right_aligned(links, 80.f);
+
+  for_all_links(l, links)
+    mln_assertion(links(l) == filtered_ref[l]);
+}
