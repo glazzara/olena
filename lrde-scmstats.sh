0a72fc36e4608dc34c8c4e8d2bfc8767f2906805
Guillaume Lazzara <z@lrde.epita.fr>
2011-09-08 17:14:13 +0200

    lrde-scmstats.sh: New.

---
+++ lrde-scmstats.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+# Copyright (C) 2011 EPITA Research and Development Laboratory (LRDE).
+#
+# This file is part of Olena.
+#
+# Olena is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2 of the License.
+#
+# Olena is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# This file is run by the LRDE autobuilder before compilation.
+# It is not meant to be distributed with Olena.
+
+set -ex
+
+# Buildbot will tell us the name of the branch being compiled using $1.
+branch=$1
+
+# We might want to filter stats for specific branches.
+# Currently gitstats does not allow that.
+#case "$branch" in
+#  # Consider these branches only.
+#  master|next|swilena|mesh-segm-skel|next-build-test) ;;
+#  # Don't upload other branches.
+#  *) exit ;;
+#esac
+
+DEST=/lrde/dload/olena/stats/
+
+gitstats . $DEST
+
+# Expose updated files.
+chmod -R a+rX $DEST
