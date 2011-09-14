7a2854173de2d3d9917ebc14e5589be6ab91445f
Roland Levillain <roland@lrde.epita.fr>
2011-09-14 18:29:37 +0200

    Make Milena's copyright headers uniform.
    	* apps/mesh-segm-skel/io.hh,
    	* apps/mesh-segm-skel/mesh-complex-max-curv-segm.cc,
    	* apps/mesh-segm-skel/mesh-complex-max-curv.cc,
    	* apps/mesh-segm-skel/mesh-complex-segm.cc,
    	* apps/mesh-segm-skel/mesh-complex-skel.cc,
    	* apps/mesh-segm-skel/mesh-max-curv.cc,
    	* apps/mesh-segm-skel/mesh-pinv-curv.cc,
    	* apps/mesh-segm-skel/mesh-segm.cc,
    	* apps/mesh-segm-skel/mesh-skel.cc,
    	* apps/mesh-segm-skel/off-to-vtk-bin.cc,
    	* apps/mesh-segm-skel/save_bin_alt.hh,
    	* apps/mesh-segm-skel/trimesh/misc.hh,
    	* apps/papers/levillain.09.ismm/complex.cc,
    	* apps/papers/levillain.09.ismm/image2d.cc,
    	* doc/benchmark/canvas.cc,
    	* doc/benchmark/median/median_bench.cc,
    	* doc/benchmark/morpho/erosion.cc,
    	* doc/examples/trash/erosion.cc,
    	* doc/examples/trash/graph.cc,
    	* doc/examples/trash/labeling_algo.cc,
    	* doc/examples/trash/mk_graph.cc,
    	* doc/oldies/2008__tour1.cc,
    	* doc/oldies/2008__tour1_extra.cc,
    	* doc/oldies/2008__tour2.cc,
    	* doc/oldies/2008__tour3.cc,
    	* doc/tools/sample_utils.hh,
    	* mln/morpho/watershed/topological.hh,
    	* trash/bgraph_image.cc,
    	* trash/bgraph_image.hh,
    	* trash/bgraph_psite.hh,
    	* trash/boost_graph.cc,
    	* trash/boost_graph.hh,
    	* trash/boost_graph_access.hh,
    	* trash/boost_graph_property.hh,
    	* trash/boost_graph_structure.hh,
    	* trash/clock_neighb.hh,
    	* trash/clock_neighb2d.cc,
    	* trash/clock_neighb2d.hh,
    	* trash/clock_neighb2d_bis.cc,
    	* trash/display_color_pretty.cc,
    	* trash/display_color_pretty.hh,
    	* trash/display_remove.hh,
    	* trash/display_save.hh,
    	* trash/display_save_and_show.hh,
    	* trash/display_show.cc,
    	* trash/display_show.hh,
    	* trash/fi_adaptor.cc,
    	* trash/fi_adaptor.hh,
    	* trash/graph_image.hh,
    	* trash/line_graph_image.hh,
    	* trash/mono_obased_rle_encode.hh,
    	* trash/mono_obased_rle_image.cc,
    	* trash/mono_obased_rle_image.hh,
    	* trash/mono_rle_encode.hh,
    	* trash/mono_rle_image.cc,
    	* trash/mono_rle_image.hh,
    	* trash/neighb_get.hh,
    	* trash/neighb_image.cc,
    	* trash/neighb_image.hh,
    	* trash/obased_rle_encode.hh,
    	* trash/obased_rle_image.cc,
    	* trash/obased_rle_image.hh,
    	* trash/p_bgraph.cc,
    	* trash/p_bgraph.hh,
    	* trash/p_bgraph_piter.hh,
    	* trash/rle_encode.hh,
    	* trash/rle_image.cc,
    	* trash/rle_image.hh,
    	* trash/save_and_show.cc,
    	* trash/sparse_encode.hh,
    	* trash/sparse_image.cc,
    	* trash/sparse_image.hh,
    	* trash/t_image.cc,
    	* trash/t_image.hh,
    	* trash/to_tiles.cc,
    	* trash/to_tiles.hh,
    	* trash/translate_image.cc,
    	* trash/translate_image.hh,
    	* trash/value_enc_image.cc,
    	* trash/value_enc_image.hh,
    	* trash/value_encode.hh:
    	Here.

--- milena/trash/p_bgraph.cc
+++ milena/trash/p_bgraph.cc
@@ -1,29 +1,27 @@
 // Copyright (C) 2007, 2008 EPITA Research and Development Laboratory (LRDE)
 //
-// This file is part of the Milena Library.  This library is free
-// software; you can redistribute it and/or modify it under the terms
-// of the GNU General Public License version 2 as published by the
-// Free Software Foundation.
+// This file is part of Olena.
 //
-// This library is distributed in the hope that it will be useful,
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this library; see the file COPYING.  If not, write to
-// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
-// Boston, MA 02111-1307, USA.
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
 //
 // As a special exception, you may use this file as part of a free
-// software library without restriction.  Specifically, if other files
+// software project without restriction.  Specifically, if other files
 // instantiate templates or use macros or inline functions from this
-// file, or you compile this file and link it with other files to
-// produce an executable, this file does not by itself cause the
-// resulting executable to be covered by the GNU General Public
-// License.  This exception does not however invalidate any other
-// reasons why the executable file might be covered by the GNU General
-// Public License.
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
 
 /// \file tests/core/site_set/p_bgraph.cc
 /// \brief Tests on mln::p_bgraph (the psite based on boost-graph).
