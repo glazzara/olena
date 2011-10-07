0b1efa21dd750a9559f5e313c6718d783efc9044
Guillaume Lazzara <z@lrde.epita.fr>
2011-10-07 17:04:22 +0200

    First draft of common documentation.
    	* configure.ac: Add new makefiles.
    
    	* doc/.gitignore,
    	* doc/Doxyfile.in,
    	* doc/DoxygenLayout.xml,
    	* doc/Makefile.am,
    	* doc/doxygen.css,
    	* doc/footer.html,
    	* doc/header.html,
    	* doc/logo.jpg,
    	* doc/mainpage.hh,
    	* doc/olena.qhcp,
    	* doc/subdoc_footer.html: New.

---
+++ doc/mainpage.hh
@@ -0,0 +1,38 @@
+/** \mainpage Documentation of Olena
+
+    <center>
+    <h1>Olena Reference Documentation</h1>
+
+    <table class="doxtable itable">
+
+    <tr>
+    <th>General Presentation</th>
+    <th>Modules</th>
+    <th>Applications</th>
+    </tr>
+    <tr>
+    <td><ul><li>What is Olena ?</li><li>Bibliography</li><li>Contributors</li></ul></td>
+    <td><ul><li>List of Available modules</li><li><a href='../../../milena/doc/user-refman/html/index.html'>Milena</a></li><li><a href='../../../scribo/doc/user-refman/html/index.html'>Scribo</a></li><li>Swilena</li></ul></td>
+    <td><ul><li>Online Demos</li><li>Papers Related Programs</li><li>Olena Powered Programs</li></ul></td>
+    </tr>
+
+
+    <tr>
+    <th>Contributing</th>
+    <th>Licensing</th>
+    <th>&nbsp;</th>
+    </tr>
+    <tr>
+    <td><ul><li>Report Bugs and Make Suggestions</li><li>Get the Sources</li><li>Online Resources</li></ul></td>
+    <td><ul><li>GNU GPL</li><li>Commercial Use</li></ul></td>
+    <td>&nbsp;</td>
+    </tr>
+
+    </table>
+
+    </center>
+
+    <br>
+ */
+
+
