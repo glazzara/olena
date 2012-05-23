db5e6de389c6b8e9518ef4de7942814aad87632e
Guillaume Lazzara <z@lrde.epita.fr>
2012-05-23 16:04:14 +0200

    Fix a bug in a tutorial example.
    	* doc/examples/split/tuto4_genericity_and_algorithms-4.cc.raw,
    	* doc/examples/split/tuto4_genericity_and_algorithms-5.cc.raw: Update.
    
    	* doc/examples/tuto4_genericity_and_algorithms.cc: Include last
    	column and last row in computation.

--- milena/doc/examples/tuto4_genericity_and_algorithms.cc
+++ milena/doc/examples/tuto4_genericity_and_algorithms.cc
@@ -92,14 +92,14 @@ int main()
   lena = duplicate(lena_bak);
   // \{
   p_array<point2d> arr;
-  for (def::coord row = geom::min_row(lena); row < geom::max_row(lena); ++row)
-    for (def::coord col = geom::min_row(lena); col < geom::max_col(lena); ++col)
+  for (def::coord row = geom::min_row(lena); row <= geom::max_row(lena); ++row)
+    for (def::coord col = geom::min_row(lena); col <= geom::max_col(lena); ++col)
       if (((row + col) % 2) == 0)
 	arr.append(point2d(row, col));
   // \}
   // \{
-  for (def::coord row = geom::min_row(lena); row < geom::max_row(lena); ++row)
-    for (def::coord col = geom::min_row(lena); col < geom::max_col(lena); ++col)
+  for (def::coord row = geom::min_row(lena); row <= geom::max_row(lena); ++row)
+    for (def::coord col = geom::min_row(lena); col <= geom::max_col(lena); ++col)
       if (((row + col) % 2) == 0)
         opt::at(lena, row, col) = literal::green;
   // \}
