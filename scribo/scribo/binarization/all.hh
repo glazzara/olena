7c295e9a97f61828690aef3c49fe57d040755742
Guillaume Lazzara <z@lrde.epita.fr>
2012-06-20 09:46:41 +0200

    scribo/binarization/all.hh: Add more includes.

--- scribo/scribo/binarization/all.hh
+++ scribo/scribo/binarization/all.hh
@@ -1,5 +1,5 @@
-// Copyright (C) 2010, 2011 EPITA Research and Development Laboratory
-// (LRDE)
+// Copyright (C) 2010, 2011, 2012 EPITA Research and Development
+// Laboratory (LRDE)
 //
 // This file is part of Olena.
 //
@@ -46,11 +46,19 @@ namespace scribo
 # include <scribo/binarization/global_threshold.hh>
 # include <scribo/binarization/global_threshold_auto.hh>
 
+# include <scribo/binarization/kim.hh>
+
 # include <scribo/binarization/local_threshold.hh>
+
+# include <scribo/binarization/niblack.hh>
+
+# include <scribo/binarization/otsu.hh>
 
 # include <scribo/binarization/sauvola.hh>
 # include <scribo/binarization/sauvola_ms.hh>
 # include <scribo/binarization/sauvola_ms_split.hh>
 # include <scribo/binarization/sauvola_threshold.hh>
+
+# include <scribo/binarization/wolf.hh>
 
 #endif // ! SCRIBO_BINARIZATION_ALL_HH
