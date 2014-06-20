5ea13e376a63f9c8835f1df3a2c2a33515e179ee
Roland Levillain <roland@lrde.epita.fr>
2014-06-20 11:05:31 +0200

    Remove trash/ directories.
    	* doc/examples/trash/accu.cc,
    	* doc/examples/trash/box.cc,
    	* doc/examples/trash/cpp_issue.cc,
    	* doc/examples/trash/erosion.cc,
    	* doc/examples/trash/for_Z.cc,
    	* doc/examples/trash/graph.cc,
    	* doc/examples/trash/image2d.cc,
    	* doc/examples/trash/image_flat.cc,
    	* doc/examples/trash/image_if.cc,
    	* doc/examples/trash/image_plain.cc,
    	* doc/examples/trash/image_safe.cc,
    	* doc/examples/trash/labeling_algo.cc,
    	* doc/examples/trash/mk_graph.cc,
    	* doc/examples/trash/p_array.2.cc,
    	* doc/examples/trash/p_array.cc,
    	* doc/examples/trash/p_image.cc,
    	* doc/examples/trash/p_key.2.cc,
    	* doc/examples/trash/p_key.cc,
    	* doc/examples/trash/p_line2d.cc,
    	* doc/examples/trash/p_mutable_array_of.cc,
    	* doc/examples/trash/p_queue.cc,
    	* doc/examples/trash/p_run.cc,
    	* doc/examples/trash/p_vaccess.cc,
    	* doc/examples/trash/proxy.cc,
    	* doc/examples/trash/sub_image.cc,
    	* doc/examples/trash/sub_image_if.cc,
    	* doc/examples/trash/tuto_bis.cc,
    	* doc/examples/trash/tuto_one.cc,
    	* doc/examples/trash/vec.cc,
    	* doc/examples/trash/win_multiple.cc,
    	* doc/examples/trash/window.cc,
    	* trash/bgraph_image.cc,
    	* trash/bgraph_image.hh,
    	* trash/bgraph_psite.hh,
    	* trash/boost_graph.cc,
    	* trash/boost_graph.hh,
    	* trash/boost_graph_access.hh,
    	* trash/boost_graph_property.hh,
    	* trash/boost_graph_structure.hh,
    	* trash/clock_neighb.hh,
    	* trash/clock_neighb2d.cc,
    	* trash/clock_neighb2d.hh,
    	* trash/clock_neighb2d_bis.cc,
    	* trash/display_color_pretty.cc,
    	* trash/display_color_pretty.hh,
    	* trash/display_remove.hh,
    	* trash/display_save.hh,
    	* trash/display_save_and_show.hh,
    	* trash/display_show.cc,
    	* trash/display_show.hh,
    	* trash/fi_adaptor.cc,
    	* trash/fi_adaptor.hh,
    	* trash/freeimageplus.mk,
    	* trash/graph_image.hh,
    	* trash/line_graph_image.hh,
    	* trash/mono_obased_rle_encode.hh,
    	* trash/mono_obased_rle_image.cc,
    	* trash/mono_obased_rle_image.hh,
    	* trash/mono_rle_encode.hh,
    	* trash/mono_rle_image.cc,
    	* trash/mono_rle_image.hh,
    	* trash/neighb_get.hh,
    	* trash/neighb_image.cc,
    	* trash/neighb_image.hh,
    	* trash/obased_rle_encode.hh,
    	* trash/obased_rle_image.cc,
    	* trash/obased_rle_image.hh,
    	* trash/p_bgraph.cc,
    	* trash/p_bgraph.hh,
    	* trash/p_bgraph_piter.hh,
    	* trash/rle_encode.hh,
    	* trash/rle_image.cc,
    	* trash/rle_image.hh,
    	* trash/save_and_show.cc,
    	* trash/sparse_encode.hh,
    	* trash/sparse_image.cc,
    	* trash/sparse_image.hh,
    	* trash/t_image.cc,
    	* trash/t_image.hh,
    	* trash/to_tiles.cc,
    	* trash/to_tiles.hh,
    	* trash/translate_image.cc,
    	* trash/translate_image.hh,
    	* trash/value_enc_image.cc,
    	* trash/value_enc_image.hh,
    	* trash/value_encode.hh:
    	Remove.
    	Addresses Trac ticket #280.

--- milena/trash/freeimageplus.mk
+++
@@ -1,9 +0,0 @@
-## To be added to Makefile.am when support for FreeImagePlus is restored.
-
-# Tests depending on the FreeImagePlus library.
-if HAVE_FREEIMAGEPLUS
-  check_PROGRAMS += fi_adaptor
-  fi_adaptor_SOURCES = fi_adaptor.cc
-  fi_adaptor_CPPFLAGS = $(AM_CPPFLAGS) $(FREEIMAGEPLUS_CPPFLAGS)
-  fi_adaptor_LDFLAGS = $(AM_LDFLAGS) $(FREEIMAGEPLUS_LDFLAGS)
-endif HAVE_FREEIMAGEPLUS
